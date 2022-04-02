5ff4a9473ec472a7928fa64bfa0b421257698e51
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-02 23:41:38 +0530

    Time: 0 ms (100.00%), Space: 50.2 MB (13.88%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -14,11 +14,12 @@
  * }
  */
 
+
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return null;
-        if(root.val > key) root.left = deleteNode(root.left, key);
-        else if(root.val < key) root.right = deleteNode(root.right, key);
+        if(key < root.val) root.left = deleteNode(root.left, key);
+        else if(key > root.val) root.right = deleteNode(root.right, key);
         else{
             // Zero or Single Child of node
             if(root.left==null || root.right==null) return root.left!=null? root.left:root.right;
@@ -33,8 +34,7 @@ class Solution {
     }
     
     public int max(TreeNode root){
-        TreeNode curr = root;
-        while(curr.right!=null)  curr = curr.right;
-        return curr.val;
+        while(root.right!=null)  root = root.right;
+        return root.val;
     }
 }
