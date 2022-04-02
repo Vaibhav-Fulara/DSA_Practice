69a7ec00be30cc2378c12b8dfef63a7c13fefed2
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-03 02:57:15 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (90.03%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,30 +14,17 @@
  * }
  */
 class Solution {
+    boolean flag = true;
     public boolean isBalanced(TreeNode root) {
-        Pair ans = analyze(root);
-        return ans.flag;
+        int ans = analyze(root);
+        return flag;
     }
     
-    public Pair analyze(TreeNode root){
-        if(root == null) return new Pair(0,true);
-        Pair left = analyze(root.left);
-        Pair right = analyze(root.right);
-        Pair ans = new Pair();
-        ans.val = Math.max(left.val, right.val) + 1;
-        ans.flag = left.flag && right.flag && Math.abs(left.val-right.val)<=1;
-        return ans;
-    }
-    
-    public class Pair{
-        int val;
-        boolean flag;
-        Pair(){};
-        Pair(int val) {this.val = val;}
-        Pair(boolean flag) {this.flag = flag;}
-        Pair(int val, boolean flag){
-            this.val = val;
-            this.flag = flag;
-        }
+    public int analyze(TreeNode root){
+        if(root == null) return 0;
+        int left = analyze(root.left);
+        int right = analyze(root.right);
+        if(flag==true) flag = Math.abs(left-right)<=1;
+        return Math.max(left, right) + 1;        
     }
 }
