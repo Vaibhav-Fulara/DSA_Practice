465d90821ae43816f52da7aaa263e1db554470b4
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-05 01:47:30 +0530

    Time: 0 ms (100.00%), Space: 46 MB (18.16%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -28,18 +28,33 @@
 //     }
 // }
 
-class Solution {
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         Queue<Integer>qu = new LinkedList<>();
+//         addNodes(root,qu,k);
+//         while(qu.size()!=1) qu.remove();
+//         return qu.remove();
+//     }
+//     public void addNodes(TreeNode root, Queue<Integer>qu, int k){
+//         if(root==null) return;
+//         if(qu.size()!=k) addNodes(root.left, qu, k);
+//         if(qu.size()!=k) qu.add(root.val);
+//         if(qu.size()!=k) addNodes(root.right, qu, k);
+//     }
+// }
+
+class Solution{
+    int iterator = 0, ans = 0;
     public int kthSmallest(TreeNode root, int k) {
-        Queue<Integer>qu = new LinkedList<>();
-        addNodes(root,qu,k);
-        while(qu.size()!=1) qu.remove();
-        return qu.remove();
-    }
-    public void addNodes(TreeNode root, Queue<Integer>qu, int k){
-        if(root==null) return;
-        if(qu.size()!=k) addNodes(root.left, qu, k);
-        if(qu.size()!=k) qu.add(root.val);
-        if(qu.size()!=k) addNodes(root.right, qu, k);
+        if(root==null) return 0;
+        
+        kthSmallest(root.left,k);
+        
+        iterator++;
+        if(k==iterator) ans=root.val;
+        
+        kthSmallest(root.right,k);
+        
+        return ans;
     }
 }
-
