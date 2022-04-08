8bdbad0cd57133112558ce25f4131956f22fff80
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-09 04:57:20 +0530

    Time: 18 ms (75.56%), Space: 53.1 MB (17.71%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,16 @@
+class KthLargest {
+    
+    PriorityQueue<Integer>pq;
+    int k=0;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int val:nums) this.add(val);
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        while(pq.size()>k) pq.remove(); 
+        return pq.peek();
+    }
+}
