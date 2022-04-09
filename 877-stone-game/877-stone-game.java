b5a96cae23dd925ca26b64fee1f4ce9b60eaffbb
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-09 22:27:29 +0530

    Time: 0 ms (100.00%), Space: 42 MB (70.06%) - LeetHub

---
+++ 877-stone-game/877-stone-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean stoneGame(int[]arr) {
+        // int alice = 0, bob = 0;
+        // int i = 0;
+        // int j = arr.length-1;
+        // while(i<j){
+        //     alice+=Math.max(arr[i], arr[j]);
+        //     if (arr[i]>arr[j]) i++; 
+        //     else if (arr[j]>arr[i]) j--;
+        //     else
+        //     bob+=Math.max(arr[i], arr[j]);
+        //     if (arr[i]>arr[j]) i++; else j--;
+        // }
+        // return alice>bob;
+        return true;
+    }
+}
