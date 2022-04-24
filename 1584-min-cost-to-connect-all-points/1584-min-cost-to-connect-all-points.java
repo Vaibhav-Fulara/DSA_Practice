3391d6d43fd5a9bdb2da844cf979899b188e39e5
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-04-24 18:30:25 +0530

    Time: 1451 ms (6.65%), Space: 107.3 MB (55.17%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,3 +1,59 @@
+// class Solution {
+//     public static class Edge{
+//         int src;
+//         int nbr;
+//         int wt;
+//         Edge(int src, int wt){
+//             this.src = src;
+//             this.wt = wt;
+//         }
+//         Edge(int src, int nbr, int wt){
+//             this.src = src;
+//             this.nbr = nbr;
+//             this.wt = wt;
+//         }
+//     }
+//     public static void addToGraph(List<Edge>[]graph, int src, int dest, int wt){
+//         graph[src].add(new Edge(src, dest, wt));
+//         graph[dest].add(new Edge(dest, src, wt));
+//     }
+   
+//     public int minCostConnectPoints(int[][]arr) {
+//         int n = arr.length;
+//         List<Edge>[]graph = new ArrayList[n];
+//         for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+//         for(int i=0; i<arr.length-1; i++){
+//             for(int j=i+1; j<arr.length; j++){
+//                 int src = i;
+//                 int dest = j;
+//                 int wt = Math.abs(arr[i][0]-arr[j][0]) + Math.abs(arr[i][1]-arr[j][1]);
+//                 addToGraph(graph, src, dest, wt);
+//             }
+//         }
+//         return (primsAlgo(graph));
+//     }
+    
+//     public static int primsAlgo(List<Edge>[]graph){
+//         int minCost = 0;
+//         int[]arr = new int[graph.length];
+//         Arrays.fill(arr,-1);
+//         PriorityQueue<Edge>pq = new PriorityQueue<>((a,b)->{return a.wt-b.wt;});
+//         pq.add(new Edge(0,0));
+//         while(pq.size()!=0){
+//             Edge temp = pq.remove();
+//             if(arr[temp.src]==-1) minCost += temp.wt;
+//             arr[temp.src] = 0;
+//             for(Edge e:graph[temp.src]){
+//                 int nbr = e.nbr;
+//                 int cost = e.wt;
+//                 if(arr[nbr]==-1) pq.add(new Edge(nbr, cost));
+//             }
+//         }
+//         return minCost;
+//     }
+// }
+
+
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
@@ -14,7 +70,7 @@ class Solution {
             visited[current[1]] = true;
             cost += current[2];
             edges += 1;
-            for (int j = 0; j< n; j++) if (!visited[j]){
+            for (int j = 0; j< n; j++) if (!visited[j]) {
                 pq.add(new int [] {current[1], j, Math.abs(points[current[1]][0] - points[j][0]) + Math.abs(points[current[1]][1] - points[j][1])});
             }
         }
