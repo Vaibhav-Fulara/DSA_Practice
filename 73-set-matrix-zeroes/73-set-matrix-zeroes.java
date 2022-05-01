218a0f978ecdf27854f0e15efafaf5e4bc7998ae
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-02 04:07:42 +0530

    Time: 1 ms (88.64%), Space: 54.1 MB (43.79%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,18 +1,10 @@
 class Solution {
     public void setZeroes(int[][]arr) {
         int n = arr.length;
-        int m = arr[0].length;
-        List<Integer>rows = new ArrayList<>();
-        List<Integer>cols = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(arr[i][j] == 0) {
-                    cols.add(j);
-                    rows.add(i); 
-                }
-            }
-        }
-        for(int i:rows) for(int j=0; j<arr[0].length; j++) arr[i][j] = 0;
-        for(int i:cols) for(int j=0; j<arr.length; j++) arr[j][i] = 0;
+        int m = arr[0].length, k=0;
+        while (k < m && arr[0][k] != 0) ++k;
+        for(int i=1; i<n; i++) for(int j=0; j<m; j++) if(arr[i][j] == 0) arr[i][0] = arr[0][j] = 0;
+        for(int i=1; i<n; i++) for(int j=m-1; j>=0; j--) if(arr[i][0]==0 || arr[0][j]==0) arr[i][j] = 0;
+        if(k<m) Arrays.fill(arr[0], 0);
     }
 }
