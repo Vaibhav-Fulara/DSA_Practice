2e6906c84ae7faffeebdf84895756a95bf8bd3f6
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-08 02:58:08 +0530

    Time: 56 ms (24.01%), Space: 70.3 MB (36.07%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,32 +1,46 @@
 // Merge Sort
 //
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         return getMerged(nums, 0, nums.length-1);
+//     }
+//     public int[] getMerged(int[]arr, int lo, int hi){
+//         if(lo==hi) return new int[]{arr[lo]};
+//         int mid = (lo+hi)/2;
+//         int[]left = getMerged(arr, lo, mid);
+//         int[]right = getMerged(arr, mid+1, hi);
+//         return mergeSorted(left,right);
+//     }
+//     public int[] mergeSorted(int[]a, int[]b){
+//         int n = a.length;
+//         int m = b.length;
+//         int x=0, y=0, i=0;
+//         int[]arr = new int[n+m];
+//         while(x<n && y<m){
+//             if(a[x]<b[y]) arr[i++] = a[x++];
+//             else arr[i++] = b[y++];
+//         }
+//         for(int q=x; q<a.length; q++) arr[i++] = a[q];
+//         for(int q=y; q<b.length; q++) arr[i++] = b[q];
+//         return arr;
+//     }
+// }
+
+
+// Heap Sort
+//
 class Solution {
     public int[] sortArray(int[] nums) {
-        return getMerged(nums, 0, nums.length-1);
-    }
-    public int[] getMerged(int[]arr, int lo, int hi){
-        if(lo==hi) return new int[]{arr[lo]};
-        int mid = (lo+hi)/2;
-        int[]left = getMerged(arr, lo, mid);
-        int[]right = getMerged(arr, mid+1, hi);
-        return mergeSorted(left,right);
-    }
-    public int[] mergeSorted(int[]a, int[]b){
-        int n = a.length;
-        int m = b.length;
-        int x=0, y=0, i=0;
-        int[]arr = new int[n+m];
-        while(x<n && y<m){
-            if(a[x]<b[y]) arr[i++] = a[x++];
-            else arr[i++] = b[y++];
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        for(int val:nums) pq.add(val);
+        for(int i=0; i<nums.length; i++){
+            nums[i] = pq.remove();
         }
-        for(int q=x; q<a.length; q++) arr[i++] = a[q];
-        for(int q=y; q<b.length; q++) arr[i++] = b[q];
-        return arr;
+        return nums;
     }
 }
 
-// Insertion Sort
+// Insertion Sort[TLE]
 //
 // class Solution {
 //     public int[] sortArray(int[]arr) {
