30f49b2cc74fe0d545af23628b98f9a42ed69d5d
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-08 02:18:05 +0530

    Time: 10 ms (88.77%), Space: 52.2 MB (79.26%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,3 +1,5 @@
+// Merge Sort
+//
 class Solution {
     public int[] sortArray(int[] nums) {
         return getMerged(nums, 0, nums.length-1);
@@ -23,3 +25,67 @@ class Solution {
         return arr;
     }
 }
+
+// Insertion Sort
+//
+// class Solution {
+//     public int[] sortArray(int[]arr) {
+//         int n = arr.length;
+//         for(int i=1; i<n; i++){
+//             for(int j=i-1; j>=0; j--){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j+1];
+//                     arr[j+1] = arr[j];
+//                     arr[j] = temp;
+//                 }
+//                 else break;
+//             }            
+//         }
+//         return arr;
+//     }
+// }
+
+// Selection Sort [TLE]
+//
+// class Solution {
+//     public int[] sortArray(int[]arr) {
+//         int n = arr.length;
+//         for(int i=0; i<n-1; i++){
+//             int min = i;
+//             for(int j=i+1; j<n; j++){
+//                 if(arr[j]<arr[min]) min = j;
+//             }
+//             int temp = arr[i];
+//             arr[i] = arr[min];
+//             arr[min] = temp;
+//         }
+//         return arr;
+//     }
+// }
+
+
+// Bubble Sort [TLE]
+//
+// class Solution {
+//     public int[] sortArray(int[]arr) {
+//         int n = arr.length;
+//         for(int i=1; i<=n-1; i++){
+//             for(int j=0; j<n-i; j++){
+//                 if(arr[j]>arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
+
+// In-Built Sort
+//
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         return Arrays.sort(nums);
+//     }
+// }
