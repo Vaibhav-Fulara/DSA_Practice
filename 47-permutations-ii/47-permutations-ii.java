eee70a1d2454f00c0e0ee40f21122e47d0bc34d9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-11 18:48:37 +0530

    Time: 3 ms (83.31%), Space: 49.4 MB (24.89%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>>al = new ArrayList<>();
+        
+        getPerm(nums, al, new ArrayList<>());
+        return al;
+    }
+    
+    public void getPerm(int[]arr, List<List<Integer>>ans, List<Integer>curr){
+        if(arr.length == 0) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        int n = arr.length;
+        int[]vis = new int[21];
+        for(int i=0; i<n; i++){
+            if(vis[arr[i]+10]==0){
+                vis[arr[i]+10] = 1;
+                curr.add(arr[i]);
+                int[]temp = new int[n-1];
+                for(int k=0; k<i; k++) temp[k] = arr[k];
+                for(int k=i+1; k<n; k++) temp[k-1] = arr[k];
+                getPerm(temp, ans, curr);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+}
