ad1a22eb38def7dc9100028f8d0f3066937fd945
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-13 09:04:24 +0530

    Time: 49 ms (60.70%), Space: 56 MB (49.86%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.java
+++ 2266-count-number-of-texts/2266-count-number-of-texts.java
@@ -22,37 +22,3 @@ class Solution {
         return (int)dp[n];
     }
 }
-
-
-
-
-// class Solution {
-//     int MOD = (int) 1e9 + 7;
-//     public int countTexts(String pressedKeys) {
-//         int n = pressedKeys.length();
-//         int[] dp = new int[n + 1];
-//         dp[0] = 1;
-        
-//         for (int i = 1; i <= n; i++) {
-//             char c = pressedKeys.charAt(i - 1);
-//             dp[i] = dp[i - 1] % MOD;
-            
-//             if (i - 2 >= 0 && pressedKeys.charAt(i - 2) == c) {
-//                 dp[i] += dp[i - 2];
-//                 dp[i] %= MOD;
-                
-//                 if (i - 3 >= 0 && pressedKeys.charAt(i - 3) == c) {
-//                     dp[i] += dp[i - 3];
-//                     dp[i] %= MOD;
-                    
-//                     if ((c == '7' || c == '9') && i - 4 >= 0 && pressedKeys.charAt(i - 4) == c) {
-//                         dp[i] += dp[i - 4];
-//                         dp[i] %= MOD;
-//                     }
-//                 }
-//             }
-//         }
-        
-//         return dp[n];
-//     }
-// }
