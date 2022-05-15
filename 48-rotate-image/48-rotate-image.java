20661fb4db0180b219431f562df2dfab03214b89
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-15 21:14:35 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (14.24%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -7,21 +7,21 @@ class Solution {
     public void transpose(int[][]arr){
         int n = arr.length;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n-i-1; j++){
+            for(int j=i; j<n; j++){
                 int temp = arr[i][j];
-                arr[i][j] = arr[n-j-1][n-i-1];
-                arr[n-j-1][n-i-1] = temp;
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
     }
     
     public void reverse(int[][]arr){
         int n = arr.length;
-        for(int i=0; i<n/2; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
                 int temp = arr[i][j];
-                arr[i][j] = arr[n-i-1][j];
-                arr[n-i-1][j] = temp;
+                arr[i][j] = arr[i][n-1-j];
+                arr[i][n-1-j] = temp;
             }
         }
     }
