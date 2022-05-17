af00ba04edfaa80a4deca0eb8a7814a0a9235ee8
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-17 06:08:38 +0530

    Time: 2 ms (73.36%), Space: 101.4 MB (55.47%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -10,28 +10,9 @@
 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode tar) {
-        Queue<TreeNode>og = new ArrayDeque<>();
-        Queue<TreeNode>copy = new ArrayDeque<>();
-        
-        TreeNode root1 = original;
-        TreeNode root2 = cloned;
-        
-        og.add(root1);
-        copy.add(root2);
-        
-        while(og.size()!=0){
-            TreeNode node1 = og.poll();
-            TreeNode node2 = copy.poll();
-            if(node1 == tar) return node2;
-            if(node1.left != null){
-                og.add(node1.left);
-                copy.add(node2.left);
-            }
-            if(node1.right != null){
-                og.add(node1.right);
-                copy.add(node2.right);
-            }
-        }
-        return null;
+        if(original == null || original == tar) return cloned;
+        TreeNode left = getTargetCopy(original.left, cloned.left, tar);
+        if(left!=null) return left;
+        return getTargetCopy(original.right, cloned.right, tar);
     }
 }
