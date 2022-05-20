2017adeb80f27ce9fac95365351b9ace57724d84
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-20 14:39:14 +0530

    Time: 188 ms (44.92%), Space: 54.2 MB (68.14%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public int fourSumCount(int[] arr1, int[] arr2, int[] arr3, int[] arr4) {
-        int n = arr1.length, count = 0, sum = 0;
+        int n = arr1.length, count = 0;
         
         HashMap<Integer, Integer>hm = new HashMap<>();
         for(int i=0; i<n; i++) hm.put(arr4[i], hm.getOrDefault(arr4[i], 0)+1);
         HashMap<Integer, Integer>hm1 = new HashMap<>();
         for(int val:hm.keySet()) for(int i=0; i<n; i++) hm1.put(val + arr3[i], hm1.getOrDefault(arr3[i] + val, 0)+hm.get(val));
-        HashMap<Integer, Integer>hm2 = new HashMap<>();
-        for(int val:hm1.keySet()) for(int i=0; i<n; i++) hm2.put(val + arr2[i], hm2.getOrDefault(arr2[i] + val, 0)+hm1.get(val));
-        HashMap<Integer, Integer>hm3 = new HashMap<>();
-        for(int val:hm2.keySet()) for(int i=0; i<n; i++) hm3.put(val + arr1[i], hm3.getOrDefault(arr1[i] + val, 0)+hm2.get(val));
         
-        return hm3.getOrDefault(0,0);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int sum = arr1[i] + arr2[j];
+                count += hm1.getOrDefault(-sum, 0);
+            }
+        }
+        
+        return count;
     }
 }
