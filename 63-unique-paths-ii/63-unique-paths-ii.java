80fc13a1ade72c307cf93f8fc7476e1beece452d
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-20 09:48:35 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (48.27%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,29 +1,28 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] arr) {
-        if(arr[0][0]==1) return 0;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
+        
+        int[][]dp = new int[n][m];
+        for(int[]arr:dp) Arrays.fill(arr,-1);
         int[][]dirs = {{0,1}, {1,0}};
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][]dp = new int[m][n];
-        return getUnique(0, 0, m-1, n-1, arr, dirs, dp);
+        return traverse(obstacleGrid, 0, 0, n-1, m-1, dirs, dp);
     }
-    public int getUnique(int sr, int sc, int dr, int dc, int[][]arr, int[][]dirs, int[][]dp){
-        if(sr==dr && sc==dc) return dp[sr][sc] = 1;
-        if(dp[sr][sc]!=0) return dp[sr][sc];
+    public int traverse(int[][]arr, int sr, int sc, int dr, int dc, int[][]dirs, int[][]dp){
+        if(sr == dr && sc == dc) return dp[sr][sc] = 1;
+        if(dp[sr][sc] != -1) return dp[sr][sc];
         else{
-            int count = 0;
-            for(int i=0; i<dirs.length; i++){
-                int x = sr+dirs[i][0];
-                int y = sc+dirs[i][1];
-                if(isValid(x, y, dr, dc) && arr[x][y]!=1){
-                    count += getUnique(x, y, dr, dc, arr, dirs, dp);
-                }
+            int ans = 0;
+            arr[sr][sc] = 1;
+            for(int[]dir:dirs){
+                int x = sr + dir[0];
+                int y = sc + dir[1];
+                if(x <= dr && y <= dc && arr[x][y]!=1) ans += traverse(arr, x, y, dr, dc, dirs, dp);
             }
-            return dp[sr][sc] = count;
+            arr[sr][sc] = 0;
+            return dp[sr][sc] = ans;
         }
-    }
-    public boolean isValid(int x, int y, int m, int n){
-        if(x<=m && x>=0 && y>=0 && y<=n) return true;
-        return false;
     }
 }
