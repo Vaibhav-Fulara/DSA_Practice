b1bee878c74684ac0570c363c6df5a8610dd6978
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-22 14:36:21 +0530

    Time: 60 ms (37.81%), Space: 53.9 MB (73.26%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -2,13 +2,20 @@ class Solution {
     public int countCollisions(String s) {
         if(s.length() == 1) return 0;
         StringBuilder sb = new StringBuilder(s);
-        int count = 0;
-        for(int i=1; i<sb.length(); i++){
+        int count = 0, rmoves = 0;
+        for(int i=0; i<sb.length(); i++){
             char ch = sb.charAt(i);
+            if(i==0){
+                if(ch=='R') rmoves++;
+                continue;
+            } 
+            
+            if(ch=='R') rmoves++;
+            
+            else if(sb.charAt(i-1) != 'R') rmoves =0;
             if(ch == 'L' && sb.charAt(i-1) == 'R'){
-                int temp = 0, idx=i-1;
-                while(idx>=0 && sb.charAt(idx--)=='R') temp++;
-                count += temp + 1;
+                count += rmoves + 1;
+                rmoves = 0;
                 sb.setCharAt(i-1, 'S');
                 sb.setCharAt(i, 'S');
             }
@@ -17,9 +24,8 @@ class Solution {
                 sb.setCharAt(i, 'S');
             }
             else if(ch == 'S' && sb.charAt(i-1) == 'R'){
-                int temp = 0, idx = i-1;
-                while(idx>=0 && sb.charAt(idx--)=='R') temp++;
-                count += temp;
+                count += rmoves;
+                rmoves = 0;
                 sb.setCharAt(i-1, 'S');
             }
         }
