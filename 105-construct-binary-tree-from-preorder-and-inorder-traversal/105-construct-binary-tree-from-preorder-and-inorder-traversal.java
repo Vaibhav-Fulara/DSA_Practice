c5a4f0fd12b2a6c30587ee8d8e1c1f85ca286918
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-24 14:19:36 +0530

    Time: 3 ms (80.91%), Space: 44.7 MB (32.73%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,21 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        HashMap<Integer, Integer>hm = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) hm.put(inorder[i], i);
+        
         int n = preorder.length;
-        return getTree(preorder, inorder, 0, n-1, 0, n-1);
+        return getTree(preorder, 0, n-1, 0, n-1, hm);
     }
-    public TreeNode getTree(int[]preorder, int[]inorder, int pre_lo, int pre_hi, int in_lo, int in_hi){
+    public TreeNode getTree(int[]preorder, int pre_lo, int pre_hi, int in_lo, int in_hi, HashMap<Integer, Integer>hm){
         if(pre_lo > pre_hi || in_lo > in_hi) return null;
         
         TreeNode head = new TreeNode(preorder[pre_lo]);
         
-        int idx = in_lo;
-        while(inorder[idx] != preorder[pre_lo]) idx++;
-        
+        int idx = hm.get(preorder[pre_lo]);
         int lhs = idx - in_lo;
         
-        head.left = getTree(preorder, inorder, pre_lo + 1, pre_lo + lhs, in_lo, idx-1);
-        head.right = getTree(preorder, inorder, pre_lo + lhs +1, pre_hi, idx+1, in_hi);
+        head.left = getTree(preorder, pre_lo + 1, pre_lo + lhs, in_lo, idx-1, hm);
+        head.right = getTree(preorder, pre_lo + lhs +1, pre_hi, idx+1, in_hi, hm);
         return head;
     }
 }
