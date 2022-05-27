6c8c92447aa5b67df68e1cbe87af287f80cf48af
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-27 17:20:55 +0530

    Time: 22 ms (69.03%), Space: 42.2 MB (82.89%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,32 +1,22 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
-    public int coinChange(int[]arr, int T) {
-        int n = arr.length;
-        int prev[]=new int[T+1]; 
-        int cur[] =new int[T+1];
-
-        for(int i=0; i<=T; i++){
-            if(i%arr[0] == 0) prev[i] = i/arr[0];
-            else prev[i] = (int)Math.pow(10,9);
-        }
-
-        for(int ind = 1; ind<n; ind++){
-            for(int target = 0; target<=T; target++){
-
-                int notTake = 0 + prev[target];
-                int take = (int)Math.pow(10,9);
-                if(arr[ind]<=target)  take = 1 + cur[target - arr[ind]];
-                cur[target] = Math.min(notTake, take);
-            }
-            prev = cur;
-        }
-
-        int ans = prev[T];
-        if(ans >=(int)Math.pow(10,9)) return -1;
-        return ans;
-    }
+    public int coinChange(int[] coins, int V) {
+        if(V == 0) return 0;
+        int M = coins.length;
+        int[]dp = new int[V+1];
+	    Arrays.fill(dp, Integer.MAX_VALUE);
+	    
+	    for(int i=0; i<=V; i++){
+	        for(int j=0; j<M; j++){
+	            if(i == coins[j]) {
+	                dp[i] = 1;
+	                continue;
+	            }
+    	        if(i > coins[j]){
+    	            int val = dp[i-coins[j]];
+    	            if(val < dp[i]) dp[i] = val + 1;
+    	        } 
+	        }
+	    }
+	    return dp[V] == Integer.MAX_VALUE? -1:dp[V];
+	}
 }
-
-
-
-
