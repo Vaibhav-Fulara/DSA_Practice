212a473bc66790a9d023f49b99fd4aeb4c777097
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-27 22:35:21 +0530

    Time: 5 ms (50.26%), Space: 44.2 MB (37.32%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -3,16 +3,22 @@ class Solution {
         int n = triangle.size();
         int[][]dp = new int[n][n];
         for(int[]val:dp) Arrays.fill(val, -1);
-        return getMinPath(triangle, 0, 0, n-1,dp);
+        return getMinPath(triangle,n-1,dp);
     }
-    public int getMinPath(List<List<Integer>>al, int sr, int sc, int dr, int[][]dp){
-        if(sr == dr) return dp[sr][sc] = al.get(sr).get(sc);
-        
-        if(dp[sr][sc] != -1) return dp[sr][sc];
-        
-        int one = getMinPath(al, sr+1, sc, dr, dp);
-        int two = getMinPath(al, sr+1, sc+1, dr, dp);
-        
-        return dp[sr][sc] = Math.min(one, two) + al.get(sr).get(sc);
+    public int getMinPath(List<List<Integer>>al, int dr, int[][]dp){
+        for(int sr=dr; sr>=0; sr--){
+            for(int sc = sr; sc>=0; sc--){
+                if(sr == dr || sc == dr){
+                    dp[sr][sc] = al.get(sr).get(sc);
+                    continue;
+                } 
+
+                int one = dp[sr+1][sc];
+                int two = dp[sr+1][sc+1];
+
+                dp[sr][sc] = Math.min(one, two) + al.get(sr).get(sc);
+            }
+        }
+        return dp[0][0];
     }
 }
