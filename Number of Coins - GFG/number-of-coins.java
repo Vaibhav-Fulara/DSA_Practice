8f45fd45bd4862fa1cf76cd56da2dac938744051
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-27 17:18:22 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.java
+++ Number of Coins - GFG/number-of-coins.java
@@ -26,7 +26,7 @@ class GfG
 class Solution{
 	public int minCoins(int coins[], int M, int V){ 
 	    int[]dp = new int[V+1];
-	    Arrays.fill(dp, Integer.MAX_VALUE - 1);
+	    Arrays.fill(dp, Integer.MAX_VALUE);
 	    
 	    for(int i=0; i<=V; i++){
 	        for(int j=0; j<M; j++){
@@ -35,11 +35,11 @@ class Solution{
 	                continue;
 	            }
     	        if(i > coins[j]){
-    	            int val = dp[i-coins[j]] + 1;
-    	            if(val< dp[i]) dp[i] = val;
+    	            int val = dp[i-coins[j]];
+    	            if(val < dp[i]) dp[i] = val + 1;
     	        } 
 	        }
 	    }
-	    return dp[V] == Integer.MAX_VALUE-1? -1:dp[V];
+	    return dp[V] == Integer.MAX_VALUE? -1:dp[V];
 	}
 }
