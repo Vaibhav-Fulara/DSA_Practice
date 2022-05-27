a3e0709077af0ea8224fbc9822c5413e617f5aba
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-27 20:25:18 +0530

    Time: 8 ms (6.76%), Space: 45.7 MB (55.99%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m = grid.length-1, n= grid[0].length-1;
+    public int minPathSum(int[][]arr) {
+        int m = arr.length-1, n= arr[0].length-1;
         int[][] dirs = {{1,0}, {0,1}};
         int[][]dp = new int[m+1][n+1];
-        for(int[]arr:dp) Arrays.fill(arr,-1);
-        return getPathSum(grid, m, n, 0, 0, dirs, dp);
-    }
-    public int getPathSum(int[][]arr, int m, int n, int sr, int sc, int[][]dirs, int[][]dp){
-        if(sr == m && sc == n) return dp[sr][sc] = arr[sr][sc];
-        if(dp[sr][sc] != -1) return dp[sr][sc];
-        int min = Integer.MAX_VALUE;
-        for(int[]dir:dirs){
-            int i = sr + dir[0];
-            int j = sc + dir[1];
-            if(i>=0 && j>=0 && i<=m && j<=n){
-                int val = getPathSum(arr, m, n, i, j, dirs, dp);
-                min = Math.min(min, val);
-            } 
+        for(int sr=m; sr>=0; sr--){
+            for(int sc=n; sc>=0; sc--){
+                if(sr == m && sc == n) {
+                    dp[sr][sc] = arr[sr][sc];
+                    continue;
+                }
+                int min = Integer.MAX_VALUE;
+                for(int[]dir:dirs){
+                    int i = sr + dir[0];
+                    int j = sc + dir[1];
+                    if(i>=0 && j>=0 && i<=m && j<=n){
+                        int val = dp[i][j];
+                        min = Math.min(min, val);
+                    } 
+                }
+                dp[sr][sc] = min + arr[sr][sc];
+            }
         }
-        return dp[sr][sc] = min + arr[sr][sc];
+        return dp[0][0];
     }
 }
