7ec297de4cd69f8287934bb98c9f2ad4cf4c8068
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-27 21:12:23 +0530

    Time: 3 ms (53.47%), Space: 46.7 MB (9.41%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -2,25 +2,27 @@ class Solution {
     public int minPathSum(int[][]arr) {
         int m = arr.length-1, n= arr[0].length-1;
         int[][] dirs = {{1,0}, {0,1}};
-        int[][]dp = new int[m+1][n+1];
+        
+        int[]prev = new int[n+1];
+            
         for(int sr=m; sr>=0; sr--){
+            int[]curr = new int[n+1];
             for(int sc=n; sc>=0; sc--){
                 if(sr == m && sc == n) {
-                    dp[sr][sc] = arr[sr][sc];
+                    curr[sc] = arr[sr][sc];
                     continue;
                 }
-                int min = Integer.MAX_VALUE;
-                for(int[]dir:dirs){
-                    int i = sr + dir[0];
-                    int j = sc + dir[1];
-                    if(i>=0 && j>=0 && i<=m && j<=n){
-                        int val = dp[i][j];
-                        min = Math.min(min, val);
-                    } 
-                }
-                dp[sr][sc] = min + arr[sr][sc];
+                int one = Integer.MAX_VALUE;
+                int two = Integer.MAX_VALUE;
+                
+                if(sr < m) one = prev[sc];
+                if(sc < n) two = curr[sc+1];
+                
+                int min = Math.min(one, two);
+                curr[sc] = min + arr[sr][sc];
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 }
