75bc62b32271db207d42f031edb9ddbc330e08d3
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-30 19:18:53 +0530

    Time: 7 ms (42.13%), Space: 44.5 MB (64.38%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,17 +1,19 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>result = new ArrayList<>();
-        getComb(result, new ArrayList<>(), candidates, target, 0);
-        return result;
+    public List<List<Integer>> combinationSum(int[] candidates, int tar) {
+        List<List<Integer>>ans = new ArrayList<>();
+        getSS(candidates, tar, ans, new ArrayList<>(), 0);
+        return ans;
     }
-    public void getComb(List<List<Integer>>result, List<Integer>al, int[]arr, int tar, int idx){
-        if(tar==0) {result.add(new ArrayList<>(al)); return;}
-        if(idx==arr.length) return;
-        if(arr[idx]<=tar){
-            al.add(arr[idx]);
-            getComb(result, al, arr, tar-arr[idx], idx);
+    public void getSS(int[]arr, int tar, List<List<Integer>>ans, List<Integer>al, int lo){
+        if(tar == 0){
+            ans.add(new ArrayList(al));
+            return;
+        }
+        for(int i=lo; i<arr.length; i++){
+            int val = arr[i];
+            al.add(val);
+            if(val <= tar) getSS(arr, tar-val, ans, al, i);
             al.remove(al.size()-1);
         }
-        getComb(result, al, arr, tar, idx+1);
     }
 }
