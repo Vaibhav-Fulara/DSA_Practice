f30209a72980bdba2517dda5b87f128ad65999ff
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-30 10:05:25 +0530

    Time: 2 ms (76.34%), Space: 44.5 MB (16.92%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>>al = new ArrayList<>();
-        getSS(nums, al, new ArrayList<>(), 0);
+        Arrays.sort(nums);
+        getSS(nums, al, 0, new ArrayList<>());
         return al;
     }
-    
-    public void getSS(int[]arr, List<List<Integer>>al, List<Integer>curr, int idx){
-        if(idx >= arr.length){al.add(new ArrayList<>(curr)); return;}
-        int temp = arr[idx];
-        curr.add(arr[idx]);
-        getSS(arr, al, curr, idx+1);
+    public void getSS(int[]arr, List<List<Integer>>al, int i, List<Integer>curr){
+        if(i == arr.length) {al.add(new ArrayList(curr)); return;}
+        
+        int j = i;
+        while(j<arr.length-1 && arr[j+1] == arr[j]) j++;
+        getSS(arr, al, j+1, curr);
+        
+        curr.add(arr[i]);
+        getSS(arr, al, i+1, curr);
         curr.remove(curr.size()-1);
-        while(idx<arr.length && arr[idx] == temp) idx++;
-        getSS(arr, al, curr, idx);
     }
 }
