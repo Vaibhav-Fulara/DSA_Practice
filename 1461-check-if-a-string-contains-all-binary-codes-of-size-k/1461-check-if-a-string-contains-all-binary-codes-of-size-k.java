241ba45f987885fe9ae0e560c1d30f98e2282964
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-31 10:52:55 +0530

    Time: 310 ms (15.63%), Space: 130.6 MB (20.83%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,31 +1,8 @@
 class Solution {
     public boolean hasAllCodes(String s, int k) {
-        int n = s.length(), count = 0;
-        if(k>n || s.length()<2) return false;
-        Set<Integer>hs = new HashSet<>();
-        for(int i=0; i<n; i++){
-            if(i+k <= n){
-                String str = s.substring(i, i+k);
-                int num = binToDeci(str);
-
-                if(!hs.contains(num)) count++;
-                // System.out.println(num +" "+no+" "+i+ " "+count);
-
-                hs.add(num);
-                if(count == Math.pow(2,k)) return true;
-            }
-        }
-        return false;
-    }
-    public int binToDeci(String s){
-        int ans = 0;
-        int mul = 1, i=s.length()-1;
-        
-        while(i != -1){
-            if(s.charAt(i) == '1') ans += mul;
-            mul *= 2;
-            i--;
-        }
-        return ans;
+        if(s.length() < k) return false;
+        Set<String> set = new HashSet();
+        for(int i = 0 ; i <= s.length() - k; i++) set.add(s.substring(i, i+k));
+        return set.size() == (int)Math.pow(2,k);
     }
 }
