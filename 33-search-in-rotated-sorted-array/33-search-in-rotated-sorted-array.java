131f6512c9dfc84663db4c4d3e0b2ddd5723a9c8
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-05-31 13:49:37 +0530

    Time: 1 ms (61.09%), Space: 42.3 MB (65.08%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,20 +1,18 @@
-public class Solution {
-    public int search(int[] arr, int target) {
-        int lo = 0;
-        int hi = arr.length - 1;
-        while (lo < hi) {
-            int mid = lo + (hi-lo) / 2;
-            if (arr[mid] == target) return mid;
-
-            if (arr[lo] <= arr[mid]) {
-                if (target >= arr[lo] && target < arr[mid])  hi = mid - 1;
-                else  lo = mid + 1;
-            } 
-            else {
-                if (target > arr[mid] && target <= arr[hi]) lo = mid + 1;
-                else  hi = mid - 1;
+class Solution {
+    public int search(int[]arr, int tar) {
+        int lo = 0, hi = arr.length-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] == tar) return mid;
+            if(arr[mid] >= arr[lo]){
+                if(arr[mid] > tar && arr[lo] <= tar) hi = mid-1;
+                else lo = mid+1;
+            }
+            else{
+                if(tar > arr[mid] && arr[hi] >= tar) lo = mid+1;
+                else hi = mid-1;
             }
         }
-        return arr[lo] == target ? lo : -1;
+        return -1;
     }
 }
