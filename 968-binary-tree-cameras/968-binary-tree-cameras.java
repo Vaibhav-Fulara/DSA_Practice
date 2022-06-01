6683e32982e45dcd47eb6c9e44bfb9fa77fcb243
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-01 15:34:16 +0530

    Time: 1 ms (74.17%), Space: 41.8 MB (88.79%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,21 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int minCount = 0;
+    int count = 0;
     public int minCameraCover(TreeNode root) {
-        String str = getCams(root);
-        if(str.equals("NM")) minCount++;
-        return minCount;
+        String str = getCount(root);
+        if(str.equals("NM")) count++;
+        return count;
     }
-    public String getCams(TreeNode node){
-        if(node == null) return "M";
-        String left = getCams(node.left);
-        String right = getCams(node.right);
-        if(left.equals("NM") || right.equals("NM")){
-            minCount++;
-            return "C";
-        }
-        if(left.equals("C") || right.equals("C")){
-            return "M";
-        }
-        else return "NM";
+    public String getCount(TreeNode root){
+        if(root == null) return "M";
+        String left = getCount(root.left);
+        String right = getCount(root.right);
+        if(left.equals("NM") || right.equals("NM")) {count++;  return "C";}
+        if(left.equals("C") || right.equals("C")) return "M";
+        return "NM";
     }
 }
