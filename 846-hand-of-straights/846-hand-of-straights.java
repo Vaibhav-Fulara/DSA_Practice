c03efdae70e343ba01b1306c25a1911c771bf2f9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-02 16:37:48 +0530

    Time: 71 ms (51.79%), Space: 43.9 MB (79.65%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.java
+++ 846-hand-of-straights/846-hand-of-straights.java
@@ -1,16 +1,15 @@
-class Solution {
-    public boolean isNStraightHand(int[]arr, int k) {
-        if(k == 1) return true;
-        Map<Integer, Integer>tm = new TreeMap<>();
-        for(int i=0; i<arr.length; i++) tm.put(arr[i], tm.getOrDefault(arr[i], 0) + 1);
-        for(int val:tm.keySet()){
-            if(tm.get(val)!=0){
-                for(int i=1; i<k; i++){
-                    if(tm.getOrDefault(val+i, 0) < tm.get(val)) return false;
-                    tm.put(val+i, tm.get(val+i)-tm.get(val));
-                }
-            }
+class Solution{
+    public boolean isNStraightHand(int[] hand, int W) {
+        Map<Integer, Integer> c = new TreeMap<>();
+        for (int i : hand) c.put(i, c.getOrDefault(i, 0)+1);
+        Queue<Integer> start = new LinkedList<>();
+        int last_checked = -1, opened = 0;
+        for (int i : c.keySet()) {
+            if (opened > 0 && i > last_checked + 1 || opened > c.get(i)) return false;
+            start.add(c.get(i) - opened);
+            last_checked = i; opened = c.get(i);
+            if (start.size() == W) opened -= start.remove();
         }
-        return true;
+        return opened == 0;
     }
 }
