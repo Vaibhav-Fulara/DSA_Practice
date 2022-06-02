0fc5fcb1290cd429c60531fe5db53a83c3c659e7
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-02 19:55:47 +0530

    Time: 93 ms (26.99%), Space: 54.1 MB (48.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -3,33 +3,20 @@ class Solution {
         int sum = 0;
         for(int val:arr) sum += val;
         if(sum % 2 != 0) return false;
+        int k = sum/2;
         
-        int[][]dp = new int[arr.length +1][sum/2 + 1];
-        for(int[]sub:dp) Arrays.fill(sub, -1);
+        boolean[][]dp = new boolean[arr.length +1][k + 1];
+        for(boolean[]val:dp) val[0] = true;
         
-        return getSS(sum/2, arr, 0, dp);
-    }
-    public boolean getSS(int tar, int[]arr, int i, int[][]dp){
-        if(tar == 0) {
-            dp[i][tar] = 1;
-            return true;
+        for(int i=arr.length-1; i>=0; i--){
+            if(arr[i] <= k) dp[i][arr[i]] = true;
+            for(int tar = 1; tar<=k; tar++){
+                boolean contain = false;
+                if(arr[i] <= tar) contain = dp[i+1][tar - arr[i]];
+                boolean ignore = dp[i+1][tar];
+                dp[i][tar] = contain || ignore;
+            }
         }
-        
-        if(i >= arr.length) {
-            dp[i][tar] = 0;
-            return false;
-        }
-        
-        if(dp[i][tar] != -1){
-            if(dp[i][tar] == 0) return false;
-            return true;
-        }
-        
-        boolean contain = false;
-        if(arr[i] <= tar) contain = getSS(tar - arr[i], arr, i+1, dp);
-        boolean ignore = getSS(tar, arr, i+1, dp);
-        boolean ans = contain || ignore;
-        dp[i][tar] = ans == true? 1:0;
-        return ans;
+        return dp[0][k];
     }
 }
