2e25bca9a011c5a37acf514336d224358ca87d46
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-03 18:46:30 +0530

    Time: 223 ms (27.90%), Space: 125.1 MB (70.26%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,33 +1,26 @@
 class NumMatrix {
-    int[][]arr, mat;
+    int[][]arr;
     public NumMatrix(int[][] matrix) {
         
         int m = matrix.length;
         int n = matrix[0].length;
         
-        arr = new int[m][n];
-        mat = matrix;
+        arr = new int[m+1][n+1];
         
         for(int i=0; i < m; i++){
-            int sum = 0;
             for(int j = 0; j<n; j++) {
-                sum += matrix[i][j];
-                arr[i][j] = sum;
+                if(i == 0 && j == 0) arr[i][j] = matrix[i][j];
+                else if(i==0) arr[i][j] = arr[i][j-1] + matrix[i][j];
+                else if(j==0) arr[i][j] = arr[i-1][j] + matrix[i][j];
+                else arr[i][j] = arr[i-1][j] + arr[i][j-1] + matrix[i][j] - arr[i-1][j-1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for(int i=row1; i <= row2; i++){
-            sum += mat[i][col1] + arr[i][col2]-arr[i][col1];
-        }
-        return sum;
+        if(row1 == 0 && col1 == 0) return arr[row2][col2];
+        if(row1 == 0) return arr[row2][col2] - arr[row2][col1-1];
+        if(col1 == 0) return arr[row2][col2] - arr[row1-1][col2];
+        return arr[row2][col2] - arr[row1-1][col2] - arr[row2][col1-1] + arr[row1-1][col1-1];
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
