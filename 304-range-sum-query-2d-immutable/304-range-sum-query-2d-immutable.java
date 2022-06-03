349e4001d08887f512a5cfac043c026cb8e30fb9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-03 18:49:47 +0530

    Time: 182 ms (45.75%), Space: 133.9 MB (15.11%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -7,20 +7,10 @@ class NumMatrix {
         
         arr = new int[m+1][n+1];
         
-        for(int i=0; i < m; i++){
-            for(int j = 0; j<n; j++) {
-                if(i == 0 && j == 0) arr[i][j] = matrix[i][j];
-                else if(i==0) arr[i][j] = arr[i][j-1] + matrix[i][j];
-                else if(j==0) arr[i][j] = arr[i-1][j] + matrix[i][j];
-                else arr[i][j] = arr[i-1][j] + arr[i][j-1] + matrix[i][j] - arr[i-1][j-1];
-            }
-        }
+        for(int i=1; i <= m; i++) for(int j = 1; j<= n; j++) arr[i][j] = arr[i-1][j] + arr[i][j-1] + matrix[i-1][j-1] - arr[i-1][j-1];
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        if(row1 == 0 && col1 == 0) return arr[row2][col2];
-        if(row1 == 0) return arr[row2][col2] - arr[row2][col1-1];
-        if(col1 == 0) return arr[row2][col2] - arr[row1-1][col2];
-        return arr[row2][col2] - arr[row1-1][col2] - arr[row2][col1-1] + arr[row1-1][col1-1];
+        return arr[row2+1][col2+1] - arr[row1][col2+1] - arr[row2+1][col1] + arr[row1][col1];
     }
 }
