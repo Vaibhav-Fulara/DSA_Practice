42e18ff4aa45b76ea2d81340e0a53ea4fc591e8e
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-03 22:53:20 +0530

    Time: 35 ms (36.72%), Space: 46.6 MB (54.66%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,32 +1,29 @@
 class Solution {
-    public int cherryPickup(int[][]arr) {
-        int n = arr.length, m = arr[0].length;
-        int[][][]dp = new int[n][m][m];
-        for(int[][]nums:dp){
-            for(int[]num:nums){
-                Arrays.fill(num,-1);
-            }
-        }
-        return getCherry(0, 0, m-1, m, n, arr, dp);
+    public int cherryPickup(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        
+        int [][][]dp = new int[n][m][m];
+        for(int [][]arr:dp) for(int[]a:arr) Arrays.fill(a,-1);
+        return getMaxCherry(grid, 0, 0, m-1, dp);
     }
-    public int getCherry(int i, int j1, int j2, int m, int n, int[][]arr, int[][][]dp){
-        if(i == n-1) {
-            if(j1 == j2) return dp[i][j1][j2] = arr[i][j1];
-            else return dp[i][j1][j2] = arr[i][j1] + arr[i][j2];
+    public int getMaxCherry(int[][]grid, int i, int j1, int j2, int[][][]dp){
+        if(i == grid.length-1){
+            if(j1 == j2) return dp[i][j1][j2] = grid[i][j1];
+            return dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
         }
         
         if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
         
-        int max = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE, curr = 0;
+        if(j1 == j2) curr += grid[i][j1];
+        else curr += grid[i][j1] + grid[i][j2];
         
-        int val = 0;
-        if(j1 == j2) val+= arr[i][j1];
-        else val+= arr[i][j1] + arr[i][j2];
-        
-        for(int dj1 = -1; dj1<=1; dj1++){
-            for(int dj2 = -1; dj2<=1; dj2++){
-                if(j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m)
-                max = Math.max(max, val+getCherry(i+1, j1+dj1, j2+dj2, m, n, arr, dp));
+        for(int dj1 = -1; dj1 <= 1; dj1++){
+            for(int dj2 = -1; dj2 <= 1; dj2++){
+                int n1 = j1 + dj1, n2 = j2 + dj2;
+                if(n1 >= 0 && n2 >= 0 && n1 < grid[0].length && n2 < grid[0].length){
+                    max = Math.max(max, curr + getMaxCherry(grid, i+1, n1, n2, dp));
+                }
             }
         }
         return dp[i][j1][j2] = max;
