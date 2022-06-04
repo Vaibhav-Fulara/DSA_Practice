695fb2e3f502673daf7aa8925f95d8c4f419451c
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-04 18:13:06 +0530

    Added solution - LeetHub

---
+++ Min Coin - GFG/min-coin.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String S = br.readLine().trim();
+            String[] S1 = S.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int amount  = Integer.parseInt(S1[1]);
+            String s = br.readLine().trim();
+            String[] s1 = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < s1.length; i++)
+                nums[i] = Integer.parseInt(s1[i]);
+            Solution ob = new Solution();
+            int ans = ob.MinCoin(nums, amount);
+            System.out.println(ans);           
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int MinCoin(int[] nums, int amount){
+        int[][]dp = new int[nums.length][amount+1];
+        for(int[]ar:dp) Arrays.fill(ar, -1);
+        int ans = getMinCoins(nums, amount, 0, dp);
+        return ans >= 1e9? -1:ans;
+    }
+    public int getMinCoins(int[]nums, int amount, int i, int[][]dp){
+        if(i == nums.length-1){
+            if(amount % nums[i] == 0) return dp[i][amount] = amount/nums[i];
+            return dp[i][amount] = (int)1e9;
+        }
+        
+        if(dp[i][amount] != -1) return dp[i][amount];
+        
+        int take = (int)1e9;
+        if(amount >= nums[i]) take = 1 + getMinCoins(nums, amount-nums[i], i, dp);
+        int notTake = getMinCoins(nums, amount, i+1, dp);
+        
+        return dp[i][amount] = Math.min(take, notTake);
+    }
+}
