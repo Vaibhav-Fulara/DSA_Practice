7800aad4c98b20c8ce6863d87783fbefc073e7c0
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-05 01:15:08 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[]){
+        int[][]dp = new int[val.length][W+1];
+        for(int[]ar:dp) Arrays.fill(ar, -1);
+        return solve(val.length-1, W, val, wt, dp);
+    }
+    static int solve(int i, int max, int[]val, int[]wt, int[][]dp){
+        if(i == 0){
+            if(wt[i] <= max) return dp[i][max] = max/wt[i]*val[i];
+            return dp[i][max] = 0;
+        }
+        
+        if(dp[i][max] != -1) return dp[i][max];
+        
+        int pick = -1;
+        if(wt[i] <= max) pick = val[i] + solve(i, max-wt[i], val, wt, dp);
+        int ignore = solve(i-1, max, val, wt, dp);
+        return dp[i][max] = Math.max(pick, ignore);
+    }
+}
