7d673d2760e085e3633d62a279f550d88bbcb2ad
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-05 00:27:44 +0530

    Time: 19 ms (20.83%), Space: 50.5 MB (39.35%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -18,6 +18,7 @@ class Solution {
 */
 
 // Memoisation
+/*
 class Solution {
     public int change(int tar, int[] arr) {
         int[][]dp = new int[arr.length][tar+1];
@@ -36,5 +37,28 @@ class Solution {
         if(arr[i] <= tar) include = getWays(i, tar-arr[i], arr, dp);
         int ignore = getWays(i-1, tar, arr, dp);
         return dp[i][tar] = include + ignore;
+    }
+}
+*/
+
+// Tabulation
+class Solution {
+    public int change(int k, int[] arr) {
+        int[][]dp = new int[arr.length][k+1];
+        
+        for(int i = 0; i<arr.length; i++){
+            for(int tar = 0; tar<=k; tar++){
+                if(i == 0){
+                    if(tar % arr[i] == 0) dp[i][tar] = 1;
+                    else dp[i][tar] = 0;
+                    continue;
+                }
+                int include = 0;
+                if(arr[i] <= tar) include = dp[i][tar-arr[i]];
+                int ignore = dp[i-1][tar];
+                dp[i][tar] = include + ignore;
+            }
+        }
+        return dp[arr.length-1][k];
     }
 }
