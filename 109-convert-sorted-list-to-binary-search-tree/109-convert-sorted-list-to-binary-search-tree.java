eda5dde6c6a73c393105bf67a6602f0e3c8726b9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-06 02:41:49 +0530

    Time: 1 ms (80.31%), Space: 46.8 MB (63.01%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.java
@@ -1,31 +1,10 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// O(n^2) 
+/*
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
+        
         if(head == null) return null;
+        
         int n = height(head);
         if(n==1) return new TreeNode(head.val);
         
@@ -57,5 +36,32 @@ class Solution {
             root = root.next;
         }
         return ans;
+    }
+}
+*/
+
+// O(nlogn)
+
+class Solution{
+    public TreeNode sortedListToBST(ListNode head){
+        if(head == null) return null;
+        return helper(head, null);
+    }
+    public TreeNode helper(ListNode head, ListNode tail){
+        if(head == tail) return null;
+        
+        ListNode slo = head;
+        ListNode fast = head;
+        
+        while(fast != tail && fast.next != tail){
+            fast = fast.next.next;
+            slo = slo.next;
+        }
+        
+        TreeNode node = new TreeNode(slo.val);
+        node.left = helper(head, slo);
+        node.right = helper(slo.next, tail);
+        
+        return node;
     }
 }
