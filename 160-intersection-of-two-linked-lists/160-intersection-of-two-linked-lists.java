5b8caf812f122c1d2fb34e5884e4315290bccaf9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-06 10:02:33 +0530

    Time: 1 ms (99.17%), Space: 55.2 MB (52.91%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -9,6 +9,10 @@
  *     }
  * }
  */
+
+// TC = O(m) + O(n)
+// SC = O(m)
+/*
 public class Solution {
     public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
         Set<ListNode>hs = new HashSet<>();
@@ -21,5 +25,44 @@ public class Solution {
             head2 = head2.next;
         }
         return null;
+    }
+}
+*/
+
+class Solution{
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        int a = size(head1);
+        int b = size(head2);
+        
+        if(a>b){
+            while(a != b){
+                head1 = head1.next;
+                a--;
+            }
+        }
+        
+        if(b>a){
+            while(a != b){
+                head2 = head2.next;
+                b--;
+            }
+        }
+        
+        while(head1 != null){
+            if(head1 == head2) return head1;
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        
+        return null;
+    }
+    
+    public int size(ListNode node){
+        int ans = 0;
+        while(node != null){
+            node = node.next;
+            ans++;
+        }
+        return ans;
     }
 }
