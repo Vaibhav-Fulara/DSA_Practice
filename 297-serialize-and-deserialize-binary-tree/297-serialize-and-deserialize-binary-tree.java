bbc7be8c2f7af878b7118c82da856e7303dda94d
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-06 22:44:31 +0530

    Time: 184 ms (12.28%), Space: 56.2 MB (13.16%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root == null) return ".";
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.val + " ");
+        
+        sb.append(serialize(root.left) + " ");
+        sb.append(serialize(root.right));
+        // System.out.println(sb);
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String str) {
+        if(str.charAt(0) == '.') return null;
+        
+        Stack<pair>st = new Stack<>();
+        String[]arr = str.split(" ");
+        
+        TreeNode root = new TreeNode (Integer.parseInt(arr[0]));
+        st.add(new pair(root, 0));
+        
+        int i=1;
+        
+        while(st.size() != 0 && i < arr.length){
+            
+            String s = arr[i];
+            System.out.println(s);
+            pair p = st.pop();
+            
+            if(p.state == 0){
+                if(s.equals(".")) {
+                    st.add(new pair(p.node, 1));
+                }
+                else {
+                    p.node.left = new TreeNode(Integer.parseInt(s));
+                    st.add(new pair(p.node, 1));
+                    st.add(new pair(p.node.left, 0));
+                }
+            }
+            
+            else if(p.state == 1){
+                if(s.equals(".")) {
+                    st.add(new pair(p.node, 2));
+                }
+                if(!s.equals(".")) {
+                    p.node.right = new TreeNode(Integer.parseInt(s));
+                    st.add(new pair(p.node.right, 0));
+                }
+            }
+            
+            else continue;
+            
+            i++;
+        }
+        
+        return root;
+    }
+}
+
+class pair{
+    TreeNode node;
+    int state;
+    pair(TreeNode node, int state){
+        this.node = node;
+        this.state = state;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
