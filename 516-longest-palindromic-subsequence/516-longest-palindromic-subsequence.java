e0fbf6e3e2727e949ff56cfbb87927dd5747ddfc
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-08 14:16:52 +0530

    Time: 94 ms (42.28%), Space: 72 MB (19.15%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -1,41 +1,28 @@
-// Recursion
-// TC = O(2^n)
-/*
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int  i = 0, j = s.length()-1;
-        return getPalindrome(s,i,j);
-    }
-    public int getPalindrome(String s, int i, int j){
-        if(i>j) return 0;
-        if(s.charAt(i) == s.charAt(j)){
-            i++; j--;
-            return 2 + getPalindrome(s, i, j);
-        }
-        else return Math.max(getPalindrome(s, i+1, j), getPalindrome(s, i, j-1));
-    }
-}
-*/
-
-// Memoization
-
-class Solution {
-    public int longestPalindromeSubseq(String s) {
-        int  i = 0, j = s.length()-1;
         
         int[][]dp = new int[s.length()][s.length()];
-        for(int[]ar:dp) Arrays.fill(ar,-1);
         
-        return getPalindrome(s, i, j, dp);
-    }
-    public int getPalindrome(String s, int i, int j, int[][]dp){
-        if(i>j) return 0;
-        if(i == j) return dp[i][j] = 1;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = 2 + getPalindrome(s, ++i, --j, dp);
+        for(int i=s.length()-1; i>=0; i--){
+            for(int j=0; j<s.length(); j++){
+                if(i>j) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                if(i == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = 2 + dp[i+1][j-1];
+                    continue;
+                }
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+            }
         }
-        else return dp[i][j] = Math.max(getPalindrome(s, i+1, j, dp), getPalindrome(s, i, j-1, dp));
+        
+        return dp[0][s.length()-1];
     }
 }
