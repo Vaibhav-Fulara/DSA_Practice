167729b42a0b969d9ac7131c66392253f0c066d7
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-09 16:53:39 +0530

    Time: 30 ms (77.69%), Space: 54.9 MB (13.39%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,3 +1,33 @@
+// Memoisation
+// TC = O(N^2)
+// SC = O(N^2) + O(N)
+/*
+class Solution {
+    public int minInsertions(String s) {
+        return s.length() - longestPalindrome(s);
+    }
+    
+    public int longestPalindrome(String s1){
+        StringBuilder sb = new StringBuilder(s1);
+        String s2 = sb.reverse().toString();
+        int[][]dp = new int[s1.length()][s2.length()];
+        for(int[]arr:dp) Arrays.fill(arr,-1);
+        return getLongestSubstring(s1, s2, 0, 0, dp);
+    }
+    
+    public int getLongestSubstring(String s1, String s2, int i, int j, int[][]dp){
+        if(i == s1.length() || j == s2.length()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = 1 + getLongestSubstring(s1, s2, i+1, j+1, dp);
+        else return dp[i][j] = Math.max(getLongestSubstring(s1, s2, i+1, j, dp), getLongestSubstring(s1, s2, i, j+1, dp));
+    }
+}
+*/
+
+// Tabulation
+// TC = O(N^2)
+// SC = O(N^2)
+/*
 class Solution {
     public int minInsertions(String s1) {
         int n = s1.length();
@@ -11,5 +41,31 @@ class Solution {
             }
         }
         return n - dp[0][0];
+    }
+}
+*/
+
+// Another approach
+
+// tabulation
+
+class Solution {
+    public int minInsertions(String s) {
+        int  i = 0, j = s.length()-1;
+        
+        int[][]dp = new int[s.length()][s.length()];
+        for(int[]ar:dp) Arrays.fill(ar,-1);
+        
+        return s.length() - getPalindrome(s, i, j, dp);
+    }
+    public int getPalindrome(String s, int i, int j, int[][]dp){
+        if(i>j) return 0;
+        if(i == j) return dp[i][j] = 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + getPalindrome(s, ++i, --j, dp);
+        }
+        else return dp[i][j] = Math.max(getPalindrome(s, i+1, j, dp), getPalindrome(s, i, j-1, dp));
     }
 }
