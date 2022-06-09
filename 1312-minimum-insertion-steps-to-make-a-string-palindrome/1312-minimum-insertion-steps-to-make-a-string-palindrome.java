32c4ff7dac74b98bb55382491c4aaa37152a3673
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-09 16:50:02 +0530

    Time: 40 ms (55.27%), Space: 53.8 MB (67.34%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,20 +1,15 @@
 class Solution {
-    public int minInsertions(String s) {
-        return s.length() - longestPalindrome(s);
-    }
-    
-    public int longestPalindrome(String s1){
+    public int minInsertions(String s1) {
+        int n = s1.length();
         StringBuilder sb = new StringBuilder(s1);
         String s2 = sb.reverse().toString();
-        int[][]dp = new int[s1.length()][s2.length()];
-        for(int[]arr:dp) Arrays.fill(arr,-1);
-        return getLongestSubstring(s1, s2, 0, 0, dp);
-    }
-    
-    public int getLongestSubstring(String s1, String s2, int i, int j, int[][]dp){
-        if(i == s1.length() || j == s2.length()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = 1 + getLongestSubstring(s1, s2, i+1, j+1, dp);
-        else return dp[i][j] = Math.max(getLongestSubstring(s1, s2, i+1, j, dp), getLongestSubstring(s1, s2, i, j+1, dp));
+        int[][]dp = new int[n+1][n+1];
+        for(int i=s2.length()-1; i>=0; i--){
+            for(int j=s1.length()-1; j>=0; j--){
+                if(s1.charAt(i) == s2.charAt(j)) dp[i][j] = 1 + dp[i+1][j+1];
+                else dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return n - dp[0][0];
     }
 }
