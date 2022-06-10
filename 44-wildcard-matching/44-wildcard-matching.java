7b2bfd2cc225808fec4c58fc0cd36b8a873ddd04
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-11 01:36:03 +0530

    Time: 60 ms (21.66%), Space: 49.1 MB (27.20%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -6,17 +6,21 @@ class Solution {
     }
     public boolean solve(String s1, String s2, int i, int j){
         if(i<0 && j<0) return true;
-        if(i<0 || j<0) return false;
+        if(j<0) return false;
+        else if(i<0){
+            for(int k=0; k<=j; k++) if(s2.charAt(k) != '*') return false;
+            return true;
+        }
         
         if(s2.charAt(j) == '?' || s2.charAt(j) == s1.charAt(i)) return solve(s1, s2, i-1, j-1);
-        else if(s2.charAt(j) == '*') return solve(s1, s2, i-1, j) || solve(s1, s2, i-1, j-1);
+        else if(s2.charAt(j) == '*') return solve(s1, s2, i-1, j) || solve(s1, s2, i, j-1);
         else return false;
     }
 }
 */
 
 // Memoization
-
+/*
 class Solution {
     public boolean isMatch(String s, String p) {
         int[][]dp  =new int[s.length()][p.length()];
@@ -46,5 +50,31 @@ class Solution {
         
         dp[i][j] = -1;
         return false;
+    }
+}
+*/
+
+// Tabulation
+
+class Solution {
+    public boolean isMatch(String s1, String s2) {
+        boolean[][]dp  =new boolean[s1.length()+1][s2.length()+1];
+        for(int i=0; i<=s1.length(); i++){
+            for(int j=0; j<=s2.length(); j++){
+                if(i == 0 && j == 0) {dp[i][j] = true; continue;}
+                else if(j==0) continue;
+                else if(i == 0){
+                    boolean flag = true;
+                    for(int k=1; k<=j; k++) if(s2.charAt(k-1) != '*') flag = false;
+                    if(flag) dp[i][j] = true;
+                    continue;
+                }
+                
+                if(s2.charAt(j-1) == '?' || s2.charAt(j-1) == s1.charAt(i-1)) dp[i][j] = dp[i-1][j-1];
+                else if(s2.charAt(j-1) == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[s1.length()][s2.length()];
     }
 }
