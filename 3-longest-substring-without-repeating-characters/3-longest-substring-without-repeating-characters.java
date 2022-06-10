2424589cda7d5a55b55cc343683c315b15c5efa1
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-10 09:04:27 +0530

    Time: 18 ms (28.28%), Space: 45.4 MB (38.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,68 +1,28 @@
-/*
-    BRUTE
-    O(N3)/O(N)
-*/
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int max = 0;
-//         for(int i=0; i<s.length(); i++){
-//             for(int j=i+1; j<=s.length(); j++){
-//                 String str = s.substring(i,j);
-//                 if(check(str)) max = Math.max(str.length(), max);
-//             }
-//         }
-//         return max;
-//     }
-    
-//     public boolean check(String str){
-//         Set<Character>hs = new HashSet<>();
-//         for(char ch:str.toCharArray()){
-//             if(hs.contains(ch)) return false;
-//             hs.add(ch);
-//         }
-//         return true;
-//     }
-// }
-
-
-/* 
-    SLIGHTLY OPTIMIZED
-    O(2N)/O(N)
-*/
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         Set<Character>hs = new HashSet<>();
-//         int l = 0, r = 0, max = 0;
-//         for(char ch:s.toCharArray()){
-//             while(hs.contains(ch)){
-//                 hs.remove(s.charAt(l));
-//                 l++;
-//             }
-//             hs.add(ch);
-//             r++;
-//             max = Math.max(max, r-l);       // we dont increment by 1 as we have already incremented r
-//         }
-//         return max;
-//     }
-// }
-
-/*
-    FULLY OPTIMIZED
-    O(N)/O(N)
-*/
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer>hm = new HashMap<>();
-        int l = 0, max = 0;
-        for(int r=0; r<s.length(); r++){
-            char ch = s.charAt(r);
-            if(hm.containsKey(ch)) l = Math.max(l, hm.get(ch)+1);
-            hm.put(ch, r);
-            max = Math.max(max, r-l+1);
+        if(s.length()<2) return s.length();
+        Set<Character>hs = new HashSet<>();
+        // hs.put(s.charAt(i));
+        int i=0, j=0, count = 0, max = 0;
+        while(j<s.length()){
+            // char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if(!hs.contains(ch2)){
+                count ++;
+                hs.add(ch2);
+                j++;
+            }
+            else{
+                max = Math.max(count, max);
+                // System.out.println(ch2);
+                while(hs.contains(ch2)){
+                    hs.remove(s.charAt(i));
+                    count--; i++;
+                }
+                hs.add(ch2);
+                j++; count++;
+            }
         }
-        return max;
+        return Math.max(max, count);
     }
 }
