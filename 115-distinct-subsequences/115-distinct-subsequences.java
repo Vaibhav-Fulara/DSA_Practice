e666065afb5ab7d8e19202a67b64ee3698e2dbd0
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-10 07:02:49 +0530

    Time: 34 ms (26.84%), Space: 50.3 MB (35.52%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,3 +1,7 @@
+// Memoization
+// TC = O(m*n)
+// SC = O(m*n) + O(m+n)
+/*
 class Solution {
     public int numDistinct(String s, String t) {
         int[][]dp = new int[s.length()][t.length()];
@@ -10,5 +14,24 @@ class Solution {
         if(dp[i][j] != -1) return dp[i][j];
         if(s1.charAt(i) == s2.charAt(j)) return dp[i][j] = getMinSubseq(s1, s2, i+1, j+1, dp) + getMinSubseq(s1, s2, i+1, j, dp);
         else return dp[i][j] = getMinSubseq(s1, s2, i+1, j, dp);
+    }
+}
+*/
+
+// Tabulation
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][]dp = new int[s.length()+1][t.length()+1];
+        
+        for(int i=0; i<dp.length; i++) dp[i][t.length()] = 1;
+            
+        for(int i = s.length()-1; i>=0; i--){
+            for(int j = t.length()-1; j>=0; j--){
+                if(s.charAt(i) == t.charAt(j)) dp[i][j] = dp[i+1][j+1] + dp[i+1][j];
+                else dp[i][j] = dp[i+1][j];
+            }
+        }
+        return dp[0][0];
     }
 }
