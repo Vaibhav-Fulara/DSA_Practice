a106a456cd446f7513903114624f6c119cf8edfb
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-11 19:01:31 +0530

    Time: 2 ms (94.88%), Space: 40.6 MB (96.60%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return s.matches(p);
-    }
-}
-/*
-class Solution {
-    public boolean isMatch(String s, String p) {
         boolean[][]dp = new boolean[s.length()+1][p.length()+1];
         for(int i=0; i<=s.length(); i++){
             for(int j = 0; j <= p.length(); j++){
                 if(i == 0 && j == 0) dp[i][j] = true;
                 else if(j == 0) dp[i][j] = false;
                 else if(i == 0){
-                    boolean flag = true;
-                    for(int k=0; k<=j; k++) if(p.charAt(j-1) != '*') flag = false;
-                    if(flag) dp[i][j] = true;
+                    if(p.charAt(j-1) != '*') dp[i][j] = false;
+                    else dp[i][j] = dp[i][j-2];
                 }
-
-                else if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.') dp[i][j] = dp[i-1][j-1];
-                else if(p.charAt(j-1) == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                else dp[i][j] = false;
+                else{
+                    if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.') dp[i][j] = dp[i-1][j-1];
+                    else if(p.charAt(j-1) == '*'){
+                        dp[i][j] = dp[i][j-2];
+                        if(s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+                    else dp[i][j] = false;
+                }
             }
         }
         return dp[s.length()][p.length()];
     }
 }
-*/
