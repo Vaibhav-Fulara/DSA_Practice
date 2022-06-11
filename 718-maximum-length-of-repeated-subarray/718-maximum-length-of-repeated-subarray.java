a05477618135d7640891ac6a6735ca5c1a6b2a73
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-11 12:02:57 +0530

    Time: 119 ms (14.48%), Space: 77.2 MB (59.76%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,15 +1,16 @@
+// Similar to Longest Common Substring Tabulation
+// Only difference is we return 0 if consecutive characters are not present
+
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int[][]dp = new int[nums1.length+1][nums2.length+1];
         int max = 0;
         for(int i=nums1.length; i>=0; i--){
             for(int j=nums2.length; j>=0; j--){
-                if(i == nums1.length || j == nums2.length) continue;
-                if(nums1[i] == nums2[j]) {
+                if(!(i == nums1.length || j == nums2.length) && nums1[i] == nums2[j]) {
                     dp[i][j] = 1 + dp[i+1][j+1];
                     max = Math.max(max, dp[i][j]);
                 }
-                else dp[i][j] = 0;
             }
         }
         return max;
