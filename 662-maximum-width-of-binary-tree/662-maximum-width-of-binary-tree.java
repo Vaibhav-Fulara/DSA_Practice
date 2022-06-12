6d8cfd134d3c0cc6c23c022adc27b7e4df982766
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-12 22:57:31 +0530

    Time: 1 ms (100.00%), Space: 43.8 MB (33.85%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -15,32 +15,36 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
         Queue<TreeNode>qu = new LinkedList<>();
         root.val = 1;
         qu.add(root);
         qu.add(null);
-        int left = -1, right = -1, max = 1;
+        boolean bool = true;
+        int max = 0, left = -1, right = -1;
+        
         while(true){
-            TreeNode temp = qu.remove();
-            if(temp == null){
-                max = Math.max(max, right-left+1);
+            TreeNode node = qu.remove();
+            
+            if(node == null){
+                max = Math.max(right - left + 1, max) ;
                 left = right = -1;
                 qu.add(null);
-                if(qu.size()==1) break;
-                continue;
+                if(qu.size() == 1) break;
             }
-            if(temp.left != null){
-                temp.left.val = temp.val*2 - 1;
-                if(left != -1) right = temp.left.val;
-                else left = right = temp.left.val;
-                qu.add(temp.left);
-            }
-            if(temp.right != null){
-                temp.right.val = temp.val*2;
-                if(left != -1) right = temp.right.val;
-                else left = right = temp.right.val;
-                qu.add(temp.right);
+            
+            else{
+                if(node.left != null){
+                    node.left.val = 2*node.val;
+                    if(left == -1) left = right = node.left.val;
+                    else right = node.left.val;
+                    qu.add(node.left);
+                }
+                if(node.right != null){
+                    node.right.val = 2*node.val + 1;
+                    if(left == -1) left = right = node.right.val;
+                    else right = node.right.val;
+                    qu.add(node.right);
+                }
             }
         }
         return max;
