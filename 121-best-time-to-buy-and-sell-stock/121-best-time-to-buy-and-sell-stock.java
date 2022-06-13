0d4a81d2c42c3a9cc039de4ffec94c1afcab8dd8
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-14 04:52:04 +0530

    Time: 6 ms (6.48%), Space: 86.2 MB (5.17%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,15 @@
 class Solution {
     public int maxProfit(int[]arr) {
         
-        int[]big = new int[arr.length];
         int[]small = new int[arr.length];
         
         for(int i=0; i<arr.length; i++){
-            int j = arr.length-1-i;
-            if(i == 0){
-                small[i] = arr[i];
-                big[j] = arr[j];
-            }
-            else{
-                small[i] = Math.min(arr[i], small[i-1]);
-                big[j] = Math.max(arr[j], big[j+1]);
-            }
+            if(i == 0)  small[i] = arr[i];
+            else small[i] = Math.min(arr[i], small[i-1]);
         }
         
         int max = 0;
-        for(int i=0; i<arr.length; i++) max = Math.max(max, big[i]-small[i]);
+        for(int i=0; i<arr.length; i++) max = Math.max(max, arr[i]-small[i]);
         return max;
     }
 }
