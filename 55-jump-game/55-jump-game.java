c4e0165a07ae603caee6e8519bb0b5c938dcf567
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-14 12:45:37 +0530

    Time: 3 ms (58.96%), Space: 68 MB (42.12%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,23 +1,6 @@
-class Solution {
-    public boolean canJump(int[]arr) {
-        boolean[]dp = new boolean[arr.length];
-        
-        for(int i = arr.length-1; i>=0; i--){
-            int val = arr[i];
-            boolean bool = false;
-            for(int idx = val; idx >= 0; idx--){
-                if(i + idx >= arr.length-1){
-                    bool = true;
-                    break;
-                }
-                bool = bool || dp[i + idx];
-            }
-            dp[i] = bool;
-        }
-        return dp[0];
-    }
-}
-
+// Memoization
+// TC = O(n) TLE
+// SC = O(n)
 /*
 class Solution {
     public boolean canJump(int[]arr) {
@@ -40,3 +23,48 @@ class Solution {
     }
 }
 */
+
+// Tabulation
+// TC = O(n)
+// SC = O(n)
+/*
+class Solution {
+    public boolean canJump(int[]arr) {
+        boolean[]dp = new boolean[arr.length];
+        
+        for(int i = arr.length-1; i>=0; i--){
+            int val = arr[i];
+            boolean bool = false;
+            for(int idx = 0; idx <= val; idx++){
+                if(i + idx >= arr.length-1){
+                    bool = true;
+                    break;
+                }
+                bool = bool || dp[i + idx];
+            }
+            dp[i] = bool;
+        }
+        return dp[0];
+    }
+}
+*/
+
+// Alternate Approach
+public class Solution {
+    public boolean canJump(int[]arr) {
+       if(arr.length < 2) return true;
+       
+       for(int curr = arr.length-2; curr>=0; curr--){        // start from n-2 as we dont care if there is a 0 element on n-1
+           // System.out.println(curr);
+           if(arr[curr] == 0){
+               int neededJumps = 1;               // Jumps required to skip the 0 value
+               while(neededJumps > arr[curr]){    // If we encounter a 0 valued index, we have to skip jumping over it
+                   neededJumps++;                 // Increasing the jump required every time we undergo an unsuccessful shift
+                   curr--;                        // shifting (decrementing value) to the left of index
+                   if(curr < 0) return false;
+               }
+           }
+       }
+       return true;
+    }
+}
