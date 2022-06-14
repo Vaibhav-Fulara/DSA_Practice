15fd05fd9bf7935cb00d461de610d3cfab109190
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-14 13:06:18 +0530

    Time: 94 ms (21.30%), Space: 49.2 MB (54.25%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,3 +1,5 @@
+// Memoisation
+/*
 class Solution {
     public int jump(int[]arr) {
         int[]dp = new int[arr.length];
@@ -12,5 +14,24 @@ class Solution {
         int val = arr[i], min = (int)1e8;
         for(int idx = 1; idx <= val; idx++) min = Math.min(min, getJumps(arr, i + idx, dp));
         return dp[i] = min + 1;
+    }
+}
+*/
+
+// Tabulation
+class Solution {
+    public int jump(int[]arr) {
+        if(arr.length < 2) return 0;
+        int[]dp = new int[arr.length];
+        
+        for(int i=arr.length-1; i>=0; i--){
+            int val = arr[i], min = (int)1e8;
+            for(int idx = 1; idx <= val; idx++){
+                if(i + idx >= arr.length-1) {min = 0; break;}
+                else min = Math.min(min, dp[i+idx]);
+            } 
+            dp[i] = min + 1;
+        }
+        return dp[0];
     }
 }
