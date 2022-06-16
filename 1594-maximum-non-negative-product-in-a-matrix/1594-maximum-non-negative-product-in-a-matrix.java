6b1f01ca5a846a03b0e8339c70593a1d6b479e84
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-16 14:06:09 +0530

    Time: 3 ms (61.26%), Space: 44.1 MB (12.61%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
@@ -1,32 +1,37 @@
 class Solution {
-    public int maxProductPath(int[][] grid) {
+    public int maxProductPath(int[][] arr) {
         
-        pair[][]dp = new pair[grid.length][grid[0].length];
+        pair[][]dp = new pair[arr.length][arr[0].length];
         
-        pair p = traverse(grid, 0, 0, dp);
+        for(int i=arr.length-1; i>=0; i--){
+            for(int j=arr[0].length-1; j>=0; j--){
+                if ( i == arr.length-1 && j == arr[0].length-1 ) {
+                    dp[i][j] = new pair(arr[i][j], arr[i][j]);
+                    continue;
+                }
+
+                pair ans = new pair();
+                pair right = new pair(Integer.MAX_VALUE, Integer.MIN_VALUE), down = new pair(Integer.MAX_VALUE, Integer.MIN_VALUE);
+                if(j < arr[0].length-1) right = dp[i][j+1];
+                if(i < arr.length-1) down = dp[i+1][j];
+
+                if(arr[i][j] >= 0){
+                    ans.max = arr[i][j] * Math.max(right.max, down.max);
+                    ans.min = arr[i][j] * Math.min(right.min, down.min);
+                } else{
+                    ans.max = arr[i][j] * Math.min(right.min, down.min);
+                    ans.min = arr[i][j] * Math.max(right.max, down.max);
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+        
+        pair p = dp[0][0];
         int val = (int)(p.max % (int)(1e9 + 7));
         return val >= 0? val : -1;
     }
-    public pair traverse(int[][]arr, int i, int j, pair[][]dp){
-        if ( i == arr.length-1 && j == arr[0].length-1 ) return dp[i][j] = new pair(arr[i][j], arr[i][j]);
-        
-        if(dp[i][j] != null) return dp[i][j];
-        
-        pair ans = new pair();
-        pair right = new pair(Integer.MAX_VALUE, Integer.MIN_VALUE), down = new pair(Integer.MAX_VALUE, Integer.MIN_VALUE);
-        if(j < arr[0].length-1) right = traverse(arr, i, j+1, dp);
-        if(i < arr.length-1) down = traverse(arr, i+1, j, dp);
-        
-        if(arr[i][j] >= 0){
-            ans.max = arr[i][j] * Math.max(right.max, down.max);
-            ans.min = arr[i][j] * Math.min(right.min, down.min);
-        } else{
-            ans.max = arr[i][j] * Math.min(right.min, down.min);
-            ans.min = arr[i][j] * Math.max(right.max, down.max);
-        }
-        
-        return dp[i][j] = ans;
-    }
+    
     public class pair{
         long min;
         long max;
