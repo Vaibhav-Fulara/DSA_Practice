cbf7b12761bc9ef0d911191c9f8947f63eb72107
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-16 21:14:56 +0530

    Time: 23 ms (8.48%), Space: 67.4 MB (13.27%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,40 +1,17 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         Set<Integer>hs = new HashSet<>();
-        int[]dp = new int[arr.length];
-        return getPath(arr, start, hs, dp);
+        return getPath(arr, start, hs);
     }
-    public boolean getPath(int[]arr, int i, Set<Integer>hs, int[]dp){
+    public boolean getPath(int[]arr, int i, Set<Integer>hs){
         if(arr[i] == 0) return true;
-        
-        if(hs.contains(i)){
-            dp[i] = -1;
-            // System.out.println(1+ " " +i);
-            return false;
-        }
-        
-        if(dp[i] != 0) return dp[i] == -1 ? false:true;
+        if(hs.contains(i)) return false;
         
         hs.add(i);
-        
-        if(i-arr[i] >= 0){
-            if(getPath(arr, i-arr[i], hs, dp)) {
-                dp[i-arr[i]] = 1;
-                return true;
-            }
-        }
-        
-        if(i+arr[i] < arr.length){
-            if(getPath(arr, i+arr[i], hs, dp)) {
-                dp[i+arr[i]] = 1;
-                return true;
-            }
-        }
-        
+        if(i-arr[i] >= 0 && getPath(arr, i-arr[i], hs)) return true;
+        if(i+arr[i] < arr.length && getPath(arr, i+arr[i], hs)) return true;
         hs.remove(i);
         
-        dp[i] = -1;
-        // System.out.println(2+ " " +i);
         return false;
     }
 }
