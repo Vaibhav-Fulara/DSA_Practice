9cb0aca3f41beeb6b45079afca24030d63f5763d
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-16 21:28:38 +0530

    Time: 3 ms (98.56%), Space: 54.3 MB (83.29%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,4 +1,5 @@
 // Recursion
+/*
 class Solution {
     public boolean canReach(int[] arr, int i) {
         if(arr[i] == 0) return true;
@@ -12,45 +13,45 @@ class Solution {
         return false;
     }
 }
+*/
 
 // Memoization
-/*
+// /*
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        Set<Integer>hs = new HashSet<>();
         int[]dp = new int[arr.length];
-        return getPath(arr, start, hs, dp);
+        return getPath(arr, start, dp);
     }
-    public boolean getPath(int[]arr, int i, Set<Integer>hs, int[]dp){
+    public boolean getPath(int[]arr, int i, int[]dp){
         if(arr[i] == 0) return true;
         
-        if(hs.contains(i)){
+        if(arr[i] < 0){
             dp[i] = -1;
             return false;
         }
         
         if(dp[i] != 0) return dp[i] == -1 ? false:true;
         
-        hs.add(i);
+        arr[i] = -arr[i];
         
-        if(i-arr[i] >= 0){
-            if(getPath(arr, i-arr[i], hs, dp)) {
-                dp[i-arr[i]] = 1;
-                return true;
-            }
-        }
-        
-        if(i+arr[i] < arr.length){
-            if(getPath(arr, i+arr[i], hs, dp)) {
+        if(i+arr[i] >= 0){
+            if(getPath(arr, i+arr[i], dp)) {
                 dp[i+arr[i]] = 1;
                 return true;
             }
         }
         
-        hs.remove(i);
+        if(i-arr[i] < arr.length){
+            if(getPath(arr, i-arr[i], dp)) {
+                dp[i-arr[i]] = 1;
+                return true;
+            }
+        }
+        
+        arr[i] = -arr[i];
         
         dp[i] = -1;
         return false;
     }
 }
-*/
+// */
