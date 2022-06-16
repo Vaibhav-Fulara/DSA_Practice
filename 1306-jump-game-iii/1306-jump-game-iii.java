b04b61d50dddaf5b92d6b9cfe193db17b9aaac47
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-16 21:25:45 +0530

    Time: 4 ms (81.08%), Space: 61.4 MB (48.47%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,17 +1,56 @@
+// Recursion
 class Solution {
-    public boolean canReach(int[] arr, int start) {
-        Set<Integer>hs = new HashSet<>();
-        return getPath(arr, start, hs);
-    }
-    public boolean getPath(int[]arr, int i, Set<Integer>hs){
+    public boolean canReach(int[] arr, int i) {
         if(arr[i] == 0) return true;
-        if(hs.contains(i)) return false;
+        if(arr[i] < 0) return false;
         
-        hs.add(i);
-        if(i-arr[i] >= 0 && getPath(arr, i-arr[i], hs)) return true;
-        if(i+arr[i] < arr.length && getPath(arr, i+arr[i], hs)) return true;
-        hs.remove(i);
+        arr[i] = -arr[i];
+        if(i+arr[i] >= 0 && canReach(arr, i+arr[i])) return true;
+        if(i-arr[i] < arr.length && canReach(arr, i-arr[i])) return true;
+        arr[i] = -arr[i];
         
         return false;
     }
 }
+
+// Memoization
+/*
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        Set<Integer>hs = new HashSet<>();
+        int[]dp = new int[arr.length];
+        return getPath(arr, start, hs, dp);
+    }
+    public boolean getPath(int[]arr, int i, Set<Integer>hs, int[]dp){
+        if(arr[i] == 0) return true;
+        
+        if(hs.contains(i)){
+            dp[i] = -1;
+            return false;
+        }
+        
+        if(dp[i] != 0) return dp[i] == -1 ? false:true;
+        
+        hs.add(i);
+        
+        if(i-arr[i] >= 0){
+            if(getPath(arr, i-arr[i], hs, dp)) {
+                dp[i-arr[i]] = 1;
+                return true;
+            }
+        }
+        
+        if(i+arr[i] < arr.length){
+            if(getPath(arr, i+arr[i], hs, dp)) {
+                dp[i+arr[i]] = 1;
+                return true;
+            }
+        }
+        
+        hs.remove(i);
+        
+        dp[i] = -1;
+        return false;
+    }
+}
+*/
