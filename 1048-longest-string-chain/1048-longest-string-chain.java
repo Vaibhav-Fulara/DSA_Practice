89057346327c9a46c0dbc956b6da353690ea49b2
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-18 11:43:43 +0530

    Time: 80 ms (33.42%), Space: 44.9 MB (78.49%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,43 +1,39 @@
-class Solution{
-    private Map<Integer, List<String>> wordLengthMap;
-    private Map<String, Integer> memo;
-
-    public int longestStrChain(String[] words) {
-        // store each word with its corresponding length
-        wordLengthMap = new HashMap<>();
-        for (String word: words) {
-            wordLengthMap.putIfAbsent(word.length(), new ArrayList<>());
-            wordLengthMap.get(word.length()).add(word);
+class Solution {
+    public int longestStrChain(String[] arr) {
+        int max = 1;
+        
+        Arrays.sort(arr, (a,b)->{
+            return a.length()-b.length();
+        });
+        
+        int[]dp = new int[arr.length];
+        Arrays.fill(dp,1);
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<i; j++){
+                if(compare(arr[j], arr[i]) && dp[j] + 1 > dp[i])  dp[i] = dp[j] + 1;
+            }
+            if(dp[i] > max) max = dp[i];
         }
-
-        int maxPath = 1;
-        memo = new HashMap<>();
-        for (String word: words) 
-            maxPath = Math.max(maxPath, dfs(word));
-
-        return maxPath;
+        return max;
     }
-
-    private int dfs(String word) {
-        if (!wordLengthMap.containsKey(word.length() + 1)) return 1; // if there are no words of the next length, we're done with this path.
-        if (memo.containsKey(word)) return memo.get(word); // if we're computed this word before, return the result.
-
-        int maxPath = 0;
-        // for each word, find all words which are 1 letter longer and see if they are valid successors.
-        List<String> nextWords = wordLengthMap.get(word.length() + 1);
-        for (String nextWord: nextWords) if (isOneOff(word, nextWord)) maxPath = Math.max(maxPath, dfs(nextWord));
-
-        memo.put(word, maxPath + 1); // store our result
-        return memo.get(word);
-    }
-
-    // returns true if two strings differ by no more than 1 letter
-    private boolean isOneOff(String a, String b) {
-        int count = 0;
-        for (int i=0, j=0; i<b.length() && j<a.length() && count <= 1; i++) {
-            if (a.charAt(j) != b.charAt(i)) count++;
-            else j++;
+            
+    public boolean compare(String s1, String s2){
+        if(s1.length() + 1 != s2.length()) return false;
+        int i=0, j=0, count = 0;
+        
+        while (i < s1.length()){
+            if(j == s2.length()) return false;
+            if(s1.charAt(i) == s2.charAt(j)) {
+                i++;
+                j++;
+            }
+            else {
+                count++;
+                j++;
+            }
         }
-        return count <= 1;
+        
+        return count <=1;  
     }
 }
