de160e798d7303e8987d1c81ac61f1f40a4d1d74
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-20 07:58:19 +0530

    Time: 147 ms (58.66%), Space: 133 MB (31.81%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,3 +1,6 @@
+// TC = O(N) + O(n) not O(n^2) as in any case, we dont traverse traversed nodes again
+// SC = O(n)
+/*
 class Solution {
     public int[] dailyTemperatures(int[] arr) {
         Stack<pair>st = new Stack<>();
@@ -20,5 +23,23 @@ class Solution {
             this.val = val;
             this.idx = idx;
         }
+    }
+}
+*/
+
+class Solution {
+    public int[] dailyTemperatures(int[] arr) {
+        Stack<Integer>st = new Stack<>();
+        int[]ans = new int[arr.length];
+        
+        for(int i=0; i<arr.length; i++){
+            while(st.size()>0 && arr[st.peek()] < arr[i]){
+                int idx = st.pop();
+                ans[idx] = i-idx;
+            }
+            st.add(i);
+        }
+        
+        return ans;
     }
 }
