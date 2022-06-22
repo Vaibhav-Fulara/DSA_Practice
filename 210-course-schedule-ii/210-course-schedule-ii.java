86013d961f1fe0574a0d1c57ac1c401dded9ed83
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-22 11:47:54 +0530

    Time: 7 ms (74.68%), Space: 49.3 MB (51.78%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -3,30 +3,38 @@ class Solution {
         int[]inDegree = new int[num];
         int[]order = new int[num];
         
-        for(int[]arr:pre) inDegree[arr[0]]++;
+        List<List<Integer>>adj = new ArrayList<>();
+        for(int i=0; i<num; i++) adj.add(new ArrayList<>());
+        for(int[]arr:pre){
+            adj.get(arr[0]).add(arr[1]);
+        }
+        
+        for(int i=0; i<adj.size(); i++){
+            List<Integer>al = adj.get(i);
+            
+            for(int val:al){
+                inDegree[val]++;
+            }
+        }
         
         ArrayDeque<Integer>qu = new ArrayDeque<>();
-        for(int i=0; i<num; i++){
-            if(inDegree[i] == 0) {
-                qu.add(i);
+        for(int v = 0; v<inDegree.length; v++){
+            if(inDegree[v] == 0) {
+                qu.add(v);
             }
         }
         
-        int idx = 0;
-        while(qu.size()!=0){
-            int v = qu.poll();
-            order[idx++] = v;
+        int idx = num-1;
+        while(qu.size() > 0){
+            int v = qu.remove();
+            order[idx--] = v;
             
-            for(int[]arr:pre){
-                if(arr[1] == v){
-                    inDegree[arr[0]]--;
-                    if(inDegree[arr[0]] == 0) {
-                        qu.add(arr[0]);
-                    }
-                }
+            for(int n:adj.get(v)){
+                inDegree[n]--;
+                if(inDegree[n] == 0) qu.add(n);
             }
         }
         
-        return idx == num? order:new int[0];
+        return idx==-1? order:new int[]{};
     }
 }
