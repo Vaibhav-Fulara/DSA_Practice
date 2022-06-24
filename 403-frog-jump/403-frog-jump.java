454c2e8f33505cdf904f8ab42b1b1cd13457addc
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-25 01:22:28 +0530

    Time: 84 ms (52.47%), Space: 124.6 MB (6.60%) - LeetHub

--- 403-frog-jump/403-frog-jump.java
+++ 403-frog-jump/403-frog-jump.java
@@ -1,3 +1,4 @@
+// Memoization AC
 class Solution {
     public boolean canCross(int[]arr) {
         if(arr[1] != 1) return false;
@@ -38,3 +39,32 @@ class Solution {
         return false;
     }
 }
+// Tabulation TLE
+/*
+class Solution {
+    public boolean canCross(int[]arr) {
+        if(arr[1] != 1) return false;
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<arr.length; i++)  hm.put(arr[i], i);
+        
+        boolean[][]dp = new boolean[arr.length][arr.length];
+        
+        for(int i=arr.length-1; i>=0; i--){
+            for(int k=arr.length-1; k>=0; k--){
+                if(i == arr.length-1){
+                    dp[i][k] = true;
+                    continue;
+                }
+
+                if(hm.containsKey(arr[i]+k)) if(dp[hm.get(arr[i]+k)][k]) {dp[i][k] = true; continue;}
+                if(k+1 < arr.length && hm.containsKey(arr[i]+k+1)) if(dp[hm.get(arr[i]+k+1)][k+1]) {dp[i][k] = true; continue;}
+                if(k>1 && hm.containsKey(arr[i]+k-1)) if(dp[hm.get(arr[i]+k-1)][k-1]) {dp[i][k] = true; continue;}
+                dp[i][k] = false;
+            }
+        }
+        
+        return dp[1][1];
+    }
+}
+*/
