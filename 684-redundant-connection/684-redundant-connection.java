ee1c467976461af2d5d3ec8c55e17924bf0091d4
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-24 11:01:57 +0530

    Time: 2 ms (39.55%), Space: 43.2 MB (79.20%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int[]parent = new int[edges.length+1];
+        int[]rank = new int[edges.length+1];
+        
+        for(int i=0; i<parent.length; i++) parent[i] = i;
+        
+        List<Integer>[]graph = new ArrayList[edges.length];
+        for(int i=0; i<edges.length; i++) graph[i] = new ArrayList<>();
+        
+        for(int[]edge:edges){
+            int s1 = find(edge[0], parent);
+            int s2 = find(edge[1], parent);
+            
+            if(s1 == s2) return edge;
+            
+            union(s1, s2, parent, rank);
+        }
+        
+        return new int[]{-1,-1};
+    }
+    
+    public int find(int x, int[]parent){
+        if(x == parent[x]) return x;
+        else return find(parent[x], parent);
+    }
+    
+    public void union(int s1, int s2, int[]parent, int[]rank){
+        if(rank[s1] > rank[s2]){
+            parent[s2] = s1;
+        } else if(rank[s2] > rank[s1]){
+            parent[s1] = s2;
+        } else{
+            rank[s2]++;
+            parent[s2] = s1;
+        }
+    }
+}
