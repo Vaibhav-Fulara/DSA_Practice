f3b45cc45ac056930a23957b1d87d5331569cee0
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-25 01:10:11 +0530

    Time: 91 ms (45.87%), Space: 119.9 MB (14.46%) - LeetHub

---
+++ 403-frog-jump/403-frog-jump.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean canCross(int[]arr) {
+        if(arr[1] != 1) return false;
+        
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            hm.put(arr[i], i);
+        }
+        
+        int[][]dp = new int[arr.length][arr.length];
+        return traverse(arr, dp, 1, 1, hm);
+    }
+    public boolean traverse(int[]arr, int[][]dp, int i, int k, Map<Integer, Integer>hm){
+        if(i == arr.length-1){
+            dp[i][k] = 1;
+            return true;
+        }
+        
+        if(dp[i][k] != 0){
+            return dp[i][k] == 1? true:false;
+        }
+        
+        dp[i][k] = 1;
+        if(hm.containsKey(arr[i]+k)){
+            // System.out.println("a" +" "+ i+" "+k);
+            if(traverse(arr, dp, hm.get(arr[i]+k), k, hm)) return true;
+        } 
+        if(hm.containsKey(arr[i]+k+1)){
+            // System.out.println("b" + " " + i+" "+k);
+            if(traverse(arr, dp, hm.get(arr[i]+k+1), k+1, hm)) return true;
+        } 
+        if(k>1 && hm.containsKey(arr[i]+k-1)) {
+            // System.out.println("c" + " " + i+" "+k);
+            if(traverse(arr, dp, hm.get(arr[i]+k-1), k-1, hm)) return true;
+        }
+        dp[i][k] = -1;
+        
+        return false;
+    }
+}
