377c885a5d9f2b2ab59c67207bb7f92d234ec0dd
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-27 21:17:12 +0530

    Time: 2 ms (64.88%), Space: 41.7 MB (91.84%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,54 @@
+class Solution {
+    public boolean equationsPossible(String[]arr) {
+        // List<Character>[]graph = new ArrayList<>();
+        int[]parent = new int[26];
+        int[]ranks = new int[26];
+        
+        Arrays.sort(arr, (a,b)->{
+            if(a.charAt(2) == '!' && b.charAt(2) == '=') return -1;
+            else return 1;
+        });
+        
+        for(int i=0; i<parent.length; i++) parent[i] = i;
+        
+        for(String s:arr){
+            char ch1 = s.charAt(0);
+            char sign = s.charAt(1);
+            char ch2 = s.charAt(3);
+            
+            if(sign == '='){
+                int s1l = find(ch1-'a', parent);
+                int s2l = find(ch2-'a', parent);
+                if(s1l != s2l) union(s1l, s2l, parent, ranks);
+            }
+        }
+        
+        for(String s:arr){
+            char ch1 = s.charAt(0);
+            char sign = s.charAt(1);
+            char ch2 = s.charAt(3);
+            
+            if(sign == '!'){
+                int s1l = find(ch1-'a', parent);
+                int s2l = find(ch2-'a', parent);
+                if(s1l == s2l) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public int find(int x, int[]parent){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x], parent);
+    }
+    
+    public void union(int a, int b, int[]parent, int[]ranks){
+        if(ranks[a] > ranks[b]) parent[b] = a;
+        else if(ranks[b] > ranks[a]) parent[a] = b;
+        else{
+            parent[a] = b;
+            ranks[b]++;
+        }
+    }
+}
