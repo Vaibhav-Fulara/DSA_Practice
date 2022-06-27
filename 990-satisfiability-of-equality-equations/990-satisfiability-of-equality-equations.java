6b7f4d102e5d09729d49a1d0de747c8e01d682fc
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-28 01:05:26 +0530

    Time: 4 ms (24.75%), Space: 42.9 MB (32.60%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean equationsPossible(String[]arr) {
-        // List<Character>[]graph = new ArrayList<>();
         int[]parent = new int[26];
         int[]ranks = new int[26];
         
         Arrays.sort(arr, (a,b)->{
-            if(a.charAt(2) == '!' && b.charAt(2) == '=') return -1;
-            else return 1;
+            if(a.charAt(1) == '!' && b.charAt(1) == '=') return 1;
+            else return -1;
         });
         
         for(int i=0; i<parent.length; i++) parent[i] = i;
@@ -16,23 +15,10 @@ class Solution {
             char sign = s.charAt(1);
             char ch2 = s.charAt(3);
             
-            if(sign == '='){
-                int s1l = find(ch1-'a', parent);
-                int s2l = find(ch2-'a', parent);
-                if(s1l != s2l) union(s1l, s2l, parent, ranks);
-            }
-        }
-        
-        for(String s:arr){
-            char ch1 = s.charAt(0);
-            char sign = s.charAt(1);
-            char ch2 = s.charAt(3);
-            
-            if(sign == '!'){
-                int s1l = find(ch1-'a', parent);
-                int s2l = find(ch2-'a', parent);
-                if(s1l == s2l) return false;
-            }
+            int s1l = find(ch1-'a', parent);
+            int s2l = find(ch2-'a', parent);
+            if(s1l != s2l && sign == '=') union(s1l, s2l, parent, ranks);
+            if(s1l == s2l && sign == '!') return false;
         }
         
         return true;
