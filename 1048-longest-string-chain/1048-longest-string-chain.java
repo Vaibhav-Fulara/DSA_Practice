8a4f40403dcf129d89c32805fd23ea8490215c37
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-27 16:43:09 +0530

    Time: 100 ms (17.27%), Space: 44.5 MB (82.77%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,39 +1,34 @@
 class Solution {
-    public int longestStrChain(String[] arr) {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a, b) -> a.length() - b.length());        
+        int n = words.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
         int max = 1;
         
-        Arrays.sort(arr, (a,b)->{
-            return a.length()-b.length();
-        });
-        
-        int[]dp = new int[arr.length];
-        Arrays.fill(dp,1);
-        
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<i; j++){
-                if(compare(arr[j], arr[i]) && dp[j] + 1 > dp[i])  dp[i] = dp[j] + 1;
+        for(int i=1; i < n; i++){
+            dp[i] = 1;
+            String currWord = words[i];
+            for(int j=0; j < i; j++){
+                String prevWord = words[j];
+                if((currWord.length() - prevWord.length() == 1) && 
+                   (prevWord.length() == lcs(prevWord, currWord)))
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
             }
-            if(dp[i] > max) max = dp[i];
+            max = Math.max(dp[i], max);
         }
         return max;
     }
-            
-    public boolean compare(String s1, String s2){
-        if(s1.length() + 1 != s2.length()) return false;
-        int i=0, j=0, count = 0;
-        
-        while (i < s1.length()){
-            if(j == s2.length()) return false;
-            if(s1.charAt(i) == s2.charAt(j)) {
+
+    int lcs(String prev, String curr){
+        int p = prev.length();
+        int c = curr.length();
+        int i=0, j=0;
+        while(i < p && j < c){
+            if(prev.charAt(i) == curr.charAt(j))
                 i++;
-                j++;
-            }
-            else {
-                count++;
-                j++;
-            }
+            j++;
         }
-        
-        return count <=1;  
-    }
+        return i;
+    }    
 }
