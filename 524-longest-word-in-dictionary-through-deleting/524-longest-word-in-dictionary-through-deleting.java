b9ed00e2dff57663d13593348b3366a98c568b5b
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-28 14:56:07 +0530

    Time: 7 ms (98.47%), Space: 51.6 MB (27.48%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.java
@@ -1,3 +1,5 @@
+// Sorting
+/*
 class Solution {
     public String findLongestWord(String s, List<String> dict) {
         Collections.sort(dict);
@@ -20,5 +22,20 @@ class Solution {
         }
         
         return word;
+    }
+}
+*/
+
+class Solution {
+    public String findLongestWord(String s, List<String> d) {
+        String res="";
+        for (String c: d)
+            if ((c.length()>res.length() || c.length()==res.length() && c.compareTo(res)<0) && isSubseq(c, s)) res=c;
+        return res;
+    }
+    public boolean isSubseq(String a, String b){
+        int i=-1, j=-1;
+        while (++i<a.length()) if ((j=b.indexOf(a.charAt(i), j+1))==-1) return false;
+        return true;
     }
 }
