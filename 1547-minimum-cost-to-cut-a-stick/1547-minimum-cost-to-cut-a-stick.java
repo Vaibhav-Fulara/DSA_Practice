33472a4a5020abe7148da80a10c4ee3e08643f0c
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-29 04:38:11 +0530

    Time: 23 ms (75.50%), Space: 42.9 MB (68.21%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -2,26 +2,21 @@ class Solution {
     public int minCost(int n, int[] cuts) {
         Arrays.sort(cuts);
         int[]arr = new int[cuts.length + 2];
-        int i=0;
-        arr[i++] = 0;
-        for(int val:cuts) arr[i++] = val;
-        arr[i] = n;
+        int t=0;
+        arr[t++] = 0;
+        for(int val:cuts) arr[t++] = val;
+        arr[t] = n;
         
         int[][]dp = new int[arr.length][arr.length];
-        for(int[]ar:dp) Arrays.fill(ar, -1);        
-        
-        return partition(1, cuts.length, arr, dp);
-    }
-    public int partition(int i, int j, int[]arr, int[][]dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1)  return dp[i][j];
-        
-        int min = Integer.MAX_VALUE;
-        
-        for(int k = i; k <= j; k++){
-            min = Math.min(min, arr[j+1] - arr[i-1] + partition(i, k-1, arr, dp) + partition(k+1, j, arr, dp));
+
+        for(int i=cuts.length; i>0; i--){
+            for(int j = i; j <= cuts.length; j++){
+                int min = Integer.MAX_VALUE;
+                for(int k = i; k <= j; k++) min = Math.min(min, arr[j+1] - arr[i-1] + dp[i][k-1] + dp[k+1][j]);
+                dp[i][j] = min;
+            }
         }
         
-        return dp[i][j] = min;
+        return dp[1][cuts.length];
     }
 }
