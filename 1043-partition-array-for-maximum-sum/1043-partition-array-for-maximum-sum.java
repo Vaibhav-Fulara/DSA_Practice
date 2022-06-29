6a94d56772bc339dda30df283d0403eb052d8cb9
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-29 19:39:46 +0530

    Time: 11 ms (33.98%), Space: 43.2 MB (16.54%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,3 +1,7 @@
+// Memoization
+// O(n*k)
+// O(n) + O(n)
+/*
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int[]dp = new int[arr.length];
@@ -17,5 +21,25 @@ class Solution {
             max = Math.max(max, len*maxi + solve(arr, j+1, k, dp));
         }
         return dp[i] = max;
+    }
+}
+*/
+
+// Tabulation
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int[]dp = new int[arr.length+1];
+        
+        for(int i=arr.length-1; i>=0; i--){
+            int max = Integer.MIN_VALUE, len = 0, maxi = Integer.MIN_VALUE;
+            for(int j=i; j < Math.min(i+k, arr.length); j++){
+                len++;
+                maxi = Math.max(maxi, arr[j]);
+                max = Math.max(max, len*maxi + dp[j+1]);
+            }
+            dp[i] = max;
+        }
+        
+        return dp[0];
     }
 }
