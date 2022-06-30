d8bde8e1722bee27d6e9ba735045dafa1fa34a1e
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-06-30 14:44:03 +0530

    Time: 5 ms (66.60%), Space: 45.6 MB (64.23%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,26 +3,13 @@ class Solution {
         int n = nums.length;
         
         Arrays.sort(nums);
-        int av1 = nums[n/2];
-        int av2 = av1;
-        int idx = (int)Math.ceil((double)n/2);
-        if(idx < nums.length) av2 = nums[idx];
+        int av = n%2 != 0? nums[n/2] : (nums[n/2] + nums[n/2-1])/2;
         
-        // System.out.println(av1 +" "+av2);
-        
-        int m1 = Math.abs(av1 - nums[0]), m2 = Integer.MAX_VALUE;
-        for(int i=1; i<nums.length; i++){
+        int steps = 0;
+        for(int i=0; i<nums.length; i++){
             int num = nums[i];
-            m1 += Math.abs(av1 - num);
+            steps += Math.abs(av - num);
         }
-        if(av1 != av2){
-            m2 = Math.abs(av2 - nums[0]);
-            for(int i=1; i<nums.length; i++){
-                int num = nums[i];
-                m2 += Math.abs(av2 - num);
-            }
-        }
-        // System.out.println(m1 +" "+m2);
-        return Math.min(m1, m2);
+        return steps;
     }
 }
