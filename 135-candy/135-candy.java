cefdf3f5529f475cb72c0583aa0b4f0d8d6599c4
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-04 16:07:12 +0530

    Time: 5 ms (45.91%), Space: 52.2 MB (28.32%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -2,23 +2,13 @@ class Solution {
     public int candy(int[] rating) {
         if(rating.length == 1) return 1;
         int[]candy = new int[rating.length];
-        candy[0] = 1;
+        Arrays.fill(candy, 1);
         for(int i=1; i<candy.length; i++){
-            if(rating[i] > rating[i-1]) {
-                candy[i] = candy[i-1] + 1;
-            } else if(rating[i] == rating[i-1]) {
-                candy[i] = 1;
-            } else {
-                if(candy[i-1] != 1){
-                    candy[i] = 1;
-                } else {
-                    int j = i;
-                    candy[j] = 1;
-                    while(j > 0 && rating[j-1] > rating[j] && candy[j-1] <= candy[j]){
-                        candy[j-1] ++;
-                        j--;
-                    }
-                }
+            if(rating[i] > rating[i-1]) candy[i] = candy[i-1] + 1;
+        }
+        for(int i=candy.length-1; i > 0; i--){
+            if(rating[i-1] > rating[i] && candy[i-1] < candy[i]+1) {
+                candy[i-1] = candy[i]+1;
             }
         }
         int candies = 0;
