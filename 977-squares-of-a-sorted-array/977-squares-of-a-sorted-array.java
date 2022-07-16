b99b07696502feee991ac679e8a3406756927db3
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-16 19:26:58 +0530

    Time: 2 ms (85.31%), Space: 55.9 MB (25.17%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,36 +1,14 @@
 class Solution {
-    public int[] sortedSquares(int[] arr) {
-        int lo = 0, hi = arr.length-1;
-        int tar = -1;
-        
-        while(lo < hi) {
-            int mid = lo + (hi-lo)/2;
-            if(arr[mid] > 0) {
-                hi = mid-1;
-            } else if (arr[mid] < 0) {
-                lo = mid + 1;
-            } else {
-                tar = mid;
-                break;
-            }
-        }
-        if(tar == -1) tar = lo;
-        
-        int i=0, j=0;
-        if(arr[tar] >= 0) {j = tar; i=tar-1;}
-        else {j = tar+1; i = tar;}
-        
+    public int[] sortedSquares(int[]arr) {
         int[]ans = new int[arr.length];
-        
-        int k=0;
-        while(i >= 0 || j < arr.length){
-            if(i == -1 || j < arr.length && (int)Math.abs(arr[i]) > (int)Math.abs(arr[j])) {
-                ans[k++] = arr[j] * arr[j++];
+        int lo = 0, hi = arr.length-1, k = hi;
+        while(lo <= hi) {
+            if(Math.abs(arr[lo]) > Math.abs(arr[hi])) {
+                ans[k--] = arr[lo]*arr[lo++];
             } else {
-                ans[k++] = arr[i] * arr[i--];
+                ans[k--] = arr[hi]*arr[hi--];
             }
         }
-        
         return ans;
     }
 }
