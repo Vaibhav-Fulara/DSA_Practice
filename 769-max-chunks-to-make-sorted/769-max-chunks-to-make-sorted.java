b6c6178a8291ea5e95b9e366f77fbe908f3c7ee1
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-20 09:04:10 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (34.73%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
@@ -1,21 +1,11 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-        
-        int[]maxToLeft = new int[n];
-        int[]minToRight = new int[n];
-        maxToLeft[0] = arr[0];
-        minToRight[n-1] = arr[n-1];
-        
-        for(int i=1; i<arr.length; i++){
-            maxToLeft[i] = Math.max(maxToLeft[i-1], arr[i]);
-            minToRight[n-1-i] = Math.min(minToRight[n-i], arr[n-i-1]);
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i=0; i<arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if(max == i) count++;
         }
-        
-        int ans = 0;
-        for(int i=0; i<n-1; i++){
-            if(maxToLeft[i] <= minToRight[i+1]) ans++;
-        }
-        return ans+1;
+        return count;
     }
 }
