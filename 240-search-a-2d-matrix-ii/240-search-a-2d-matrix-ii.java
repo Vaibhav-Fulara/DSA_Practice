8ba4419eb4e36f77c07fcef4b975601453e1708f
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-24 13:49:59 +0530

    Time: 13 ms (33.20%), Space: 58.3 MB (19.35%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,12 +1,24 @@
-public class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length < 1 || matrix[0].length <1) return false;
-        int col = matrix[0].length-1;
-        int row = 0;
-        while(col >= 0 && row <= matrix.length-1) {
-            if(target == matrix[row][col]) return true;
-            else if(target < matrix[row][col]) col--;
-            else if(target > matrix[row][col]) row++;
+class Solution {
+    public boolean searchMatrix(int[][]arr, int tar) {
+        for(int i=0; i<arr.length; i++) {
+            if((arr[i][0] < tar) && (arr[i][arr[0].length-1] > tar)) {
+                if(bin(arr[i], tar)) return true;
+            } else if (arr[i][0] == tar || arr[i][arr[0].length-1] == tar) return true;
+        }
+        return false;
+    }
+    public boolean bin(int[]arr, int tar) {
+        int lo = 0;
+        int hi = arr.length;
+        while(hi >= lo) {
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] > tar) {
+                hi = mid-1;
+            } else if (arr[mid] < tar) {
+                lo = mid+1;
+            } else {
+                return true;
+            }
         }
         return false;
     }
