766ef598e1bc1fb5f7919020bca27e2926a6065f
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-25 22:27:46 +0530

    Time: 4 ms (72.57%), Space: 87.6 MB (72.76%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,10 +1,12 @@
+// O(n) || O(n)
+/*
 class Solution {
     public int partitionDisjoint(int[]arr) {
         int n = arr.length;
         int[]smolFromRight = new int[n];
         int[]bigFromLeft = new int[n];
         int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-        int part = n-1;
+        int part = n;
         
         for(int i=0; i<arr.length; i++){
             bigFromLeft[i] = Math.max(max, arr[i]);
@@ -12,10 +14,6 @@ class Solution {
             min = Math.min(min, arr[n-i-1]);
             max = Math.max(max, arr[i]);
         }
-        
-        // for(int i=0; i<arr.length; i++) {
-        //     System.out.println(bigFromLeft[i] +" "+smolFromRight[i]);
-        // }
         
         for(int i=0; i<arr.length-1; i++) {
             if(bigFromLeft[i] <= smolFromRight[i+1]) {
@@ -27,3 +25,21 @@ class Solution {
         return part;
     }
 }
+*/
+
+// O(1) || O(1)
+class Solution {
+    public int partitionDisjoint(int[]arr) {
+        int mtp = arr[0];
+        int max = arr[0];
+        int part = 1;
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i] < mtp) {
+                mtp = max;
+                part = i+1;
+            }
+            max = Math.max(arr[i], max);
+        }
+        return part;
+    }
+} 
