74373af948ea569b7fa5ea9a3f4e3da9e24bd032
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-27 23:52:00 +0530

    Time: 1 ms (86.78%), Space: 39.2 MB (91.86%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.java
+++ 670-maximum-swap/670-maximum-swap.java
@@ -1,3 +1,5 @@
+// O(nlogn) sorting
+/*
 class Solution {
     public int maximumSwap(int n) {
         List<Integer>al = new ArrayList<>();
@@ -33,6 +35,47 @@ class Solution {
         int a1 = al.get(j);
         al.set(j, al.get(broke));
         al.set(broke, a1);
+        
+        int ans = 0;
+        for(int i=0; i<al.size(); i++) {
+            ans *= 10;
+            ans += al.get(i);
+        }
+        return ans;
+    }
+}
+*/
+
+// O(n) prefix array
+class Solution {
+    public int maximumSwap(int n) {
+        List<Integer>al = new ArrayList<>();
+        List<Integer>helper = new ArrayList<>();        
+        
+        do{
+            al.add(0, n % 10);
+            n /= 10;
+        } while(n > 0);
+        
+        helper.add(al.size()-1);
+        
+        int max = al.get(al.size()-1), maxi = al.size()-1;
+        for(int i=al.size()-2; i>=0; i--) {
+            helper.add(0, maxi);
+            if(al.get(i) > max) {
+                max = al.get(i);
+                maxi = i;
+            }
+        }
+        
+        for(int i=0; i<al.size(); i++) {
+            if(al.get(i) < al.get(helper.get(i))) {
+                int temp = al.get(i);
+                al.set(i, al.get(helper.get(i)));
+                al.set(helper.get(i), temp);
+                break;
+            }
+        }
         
         int ans = 0;
         for(int i=0; i<al.size(); i++) {
