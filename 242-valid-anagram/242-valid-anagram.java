b7c0204a0c19d1c6b7584ad7892cc2cf159a7d60
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-07-28 22:45:39 +0530

    Time: 36 ms (8.96%), Space: 45.8 MB (27.10%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer>hm1 = new HashMap<>();
+        Map<Character, Integer>hm2 = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(!hm1.containsKey(ch1)) hm1.put(ch1, 0);
+            if(!hm2.containsKey(ch2)) hm2.put(ch2, 0);
+            hm1.put(ch1, hm1.get(ch1)+1);
+            hm2.put(ch2, hm2.get(ch2)+1);
+        }
+        for(char ch:hm1.keySet()) {
+            if(!hm2.containsKey(ch)) {
+                return false;
+            }
+            if(hm1.get(ch) - hm2.get(ch) != 0) {
+                // System.out.println(hm1.get(ch) + "   " + hm2.get(ch));
+                return false;
+            }
+        }
+        return true;
+    }
+}
