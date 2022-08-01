7fb9bafd2f8c7ab0ea7a064a83b73ba9799fb99a
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-01 20:12:50 +0530

    Time: 20 ms (78.67%), Space: 118.5 MB (69.93%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,3 +1,6 @@
+// Memoisation
+// O(n) / O(n)
+/*
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         Integer[]dp = new Integer[stoneValue.length];
@@ -15,5 +18,31 @@ class Solution {
         if(i < arr.length-1) max = Math.max(max, arr[i] + arr[i+1] - solve(arr, i+2, dp));
         if(i < arr.length-2) max = Math.max(max, arr[i] + arr[i+1] + arr[i+2] - solve(arr, i+3, dp));
         return dp[i] = max;
+    }
+}
+*/
+
+// Tabulation
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        if(n == 0) return "Tie";
+        Integer[]dp = new Integer[n+1];
+        int val = solve(stoneValue, dp);
+        if(val == 0) return "Tie";
+        return val > 0 ? "Alice":"Bob";
+    }
+    public int solve(int[]arr, Integer[]dp) {        
+        dp[dp.length-2] = arr[arr.length-1];
+        dp[dp.length-1] = 0;
+        for(int i=arr.length-2; i>=0; i--) {
+            int max = Integer.MIN_VALUE;
+            max = Math.max(max, arr[i] - dp[i+1]);
+            if(i < arr.length-1) max = Math.max(max, arr[i] + arr[i+1] - dp[i+2]);
+            if(i < arr.length-2) max = Math.max(max, arr[i] + arr[i+1] + arr[i+2] - dp[i+3]);
+            dp[i] = max;
+        }
+        
+        return dp[0];
     }
 }
