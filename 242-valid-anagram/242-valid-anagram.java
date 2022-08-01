468aa0804ab5e2edda0a8a8c1c6df0a3e726d37a
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-02 00:25:18 +0530

    Time: 39 ms (6.80%), Space: 46.5 MB (18.26%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -14,7 +14,6 @@ class Solution {
         for(char ch:hm1.keySet()) {
             if(!hm2.containsKey(ch)) return false;
             if(!hm1.get(ch).equals(hm2.get(ch))) {
-                System.out.println(hm1.get(ch) + "   " + hm2.get(ch));
                 return false;
             }
         }
