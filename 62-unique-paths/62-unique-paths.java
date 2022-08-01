18bb895b21614fb036c2e252e0f0619a586160f4
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-01 17:29:02 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (79.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,3 +1,5 @@
+// DP
+/*
 class Solution {
     public int uniquePaths(int m, int n) {
         Integer[][]dp = new Integer[m][n];
@@ -10,5 +12,25 @@ class Solution {
         if(j < y) ans += getPaths(i, j+1, x, y, dp);
         if(i < x) ans += getPaths(i+1, j, x, y, dp);
         return dp[i][j] = ans;
+    }
+}
+*/
+
+// Combinatorics
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) return 0;
+        if (m == 1 || n == 1) return 1;
+        if (m < n) return uniquePaths(n, m);
+
+        m--;
+        n--;
+        long paths = 1;
+        for (int i = 1; i <= n; i++) {
+            paths *= (m + i);
+            paths /= i;
+        }
+
+        return (int) paths;
     }
 }
