1a1768e076a10ee93dd4b7f629b0b5ca8e7a83bd
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-01 17:19:05 +0530

    Time: 1 ms (55.39%), Space: 40.7 MB (65.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,26 +1,14 @@
 class Solution {
-    public int uniquePaths(int m, int n){
-        int[][]dirs = {{0,1}, {1,0}};
-        int[][]dp = new int[m][n];
-        return getUnique(0, 0, m-1, n-1, dirs, dp);
+    public int uniquePaths(int m, int n) {
+        Integer[][]dp = new Integer[m][n];
+        return getPaths(0, 0, m-1, n-1, dp);
     }
-    public int getUnique(int sr, int sc, int dr, int dc, int[][]dirs, int[][]dp){
-        if(sr==dr && sc==dc) return dp[sr][sc] = 1;
-        if(dp[sr][sc]!=0) return dp[sr][sc];
-        else{
-            int count = 0;
-            for(int i=0; i<dirs.length; i++){
-                int x = sr+dirs[i][0];
-                int y = sc+dirs[i][1];
-                if(isValid(x, y, dr, dc)){
-                    count += getUnique(x, y, dr, dc, dirs, dp);
-                }
-            }
-            return dp[sr][sc] = count;
-        }
-    }
-    public boolean isValid(int x, int y, int m, int n){
-        if(x<=m && x>=0 && y>=0 && y<=n) return true;
-        return false;
+    public int getPaths(int i, int j, int x, int y, Integer[][]dp) {
+        if(i == x && j == y) return 1;
+        if(dp[i][j] != null) return dp[i][j];
+        int ans = 0;
+        if(j < y) ans += getPaths(i, j+1, x, y, dp);
+        if(i < x) ans += getPaths(i+1, j, x, y, dp);
+        return dp[i][j] = ans;
     }
 }
