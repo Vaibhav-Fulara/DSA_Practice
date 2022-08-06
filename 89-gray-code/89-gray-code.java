8208dc9075b1dbbdfc21f2def3a039b20149922d
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-07 02:13:06 +0530

    Time: 10 ms (59.96%), Space: 56.9 MB (21.45%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -1,18 +1,24 @@
 class Solution {
+    final static Map<Integer, List<Integer>>hm = new HashMap<>();
     public List<Integer> grayCode(int n) {
+        if(hm.containsKey(n)) return hm.get(n);
         if(n == 1) {
             List<Integer>ans = new ArrayList<>();
             ans.add(0);
             ans.add(1);
+            hm.put(1, ans);
             return ans;
         }
-        List<Integer>al = grayCode(n-1);
+        List<Integer>a = grayCode(n-1);
+        List<Integer>al = new ArrayList<>();
+        al.addAll(a);
         int i = al.size()-1;
         int bitmask = 1 << (n-1);
         while(i >= 0) {
             int num = al.get(i--);
             al.add(num | bitmask);
         }
+        hm.put(n, al);
         return al;
     }
 }
