667976d7349b3dd4b5acbcbd0538c63756f5737a
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-06 22:38:36 +0530

    Time: 49 ms (50.47%), Space: 70.3 MB (17.57%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,9 +1,14 @@
+// O(n^2)
+/*
 class Solution {
+    
     public int minimumDeletions(String s) {
         Integer[][]dp = new Integer[s.length()][2];
         return solve(s, 0, 0, dp);
     }
+    
     public int solve(String s, int i, int flag, Integer[][]dp) {
+        
         if(i == s.length()) return 0;
         int min = Integer.MAX_VALUE;
         
@@ -11,7 +16,7 @@ class Solution {
         
         if(flag == 0 && s.charAt(i) != 'a') {
             min = Math.min(min, 1 + solve(s, i+1, 0, dp));      // delete a and keep flag unchanged
-            min = Math.min(min, solve(s, i+1, 1, dp));      // delete b and swap the flag variable
+            min = Math.min(min, solve(s, i+1, 1, dp));          // delete b and swap the flag variable
         } else if(flag == 1 && s.charAt(i) != 'b') {
             min = 1 + solve(s, i+1, flag, dp);          // only option is to delete b
         } else {
@@ -19,5 +24,25 @@ class Solution {
         }
         
         return dp[i][flag] = min;
+    }
+}
+*/
+
+// O(n)
+class Solution {
+    public int minimumDeletions(String s) {
+        int l = s.length();
+        int[] dp = new int[l + 1];
+        int bcount = 0;
+        for (int i = 0; i < l; i++) {
+            if (s.charAt(i) == 'a') {
+                dp[i + 1] = Math.min(dp[i] + 1, bcount);
+            } else {
+                dp[i + 1] = dp[i];
+                bcount++;
+            }
+        }
+        
+        return dp[l];
     }
 }
