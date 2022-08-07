61dbae5b34a2fa88202a88b2da2af5a7c42ccb50
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-07 05:50:35 +0530

    Time: 37 ms (31.40%), Space: 41.5 MB (40.27%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -2,14 +2,12 @@ class Solution {
     public int findTheWinner(int n, int k) {
         Queue<Integer>qu = new ArrayDeque<>();
         for(int i=1; i<=n; i++) qu.add(i);
-        return solve(qu, k);
-    }
-    public int solve(Queue<Integer>qu, int k) {
-        if(qu.size() == 1) return qu.remove();
-        for(int i=1; i<k; i++) {
-            qu.add(qu.remove());
+        while(! (qu.size() == 1)) {
+            for(int i=1; i<k; i++) {
+                qu.add(qu.remove());
+            }
+            qu.remove();
         }
-        qu.remove();
-        return solve(qu, k);
+        return qu.remove();
     }
 }
