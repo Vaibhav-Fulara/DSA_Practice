7a102557e15c868f2a4263b6b6a484a07c979416
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-07 20:01:17 +0530

    Time: 69 ms (23.53%), Space: 53.8 MB (32.05%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,10 +1,57 @@
+// Recursion
+// O(5^n) || O(n^2)
+/*
 class Solution {
     final int big = (int)(1e9+7);
+    public int countVowelPermutation(int n) {
+        char[]arr = new char[]{'a', 'e', 'i', 'o', 'u'};
+        return solve(n, arr, -1);
+    }
+    public int solve(int n, char[]arr, int i) {
+        if(n == 0) return 1;
+        
+        long ans = 0l;
+        
+        if(i == 0) {
+            ans = solve(n-1, arr, 1) % big;
+        } else if(i == 1) {
+            ans += solve(n-1, arr, 0) % big;
+            ans += solve(n-1, arr, 2) % big;
+        } else if(i == 2) {
+            for(int k=0; k<5; k++) {
+                if(k == 2) continue;
+                ans += solve(n-1, arr, k);
+                ans %= big;
+            }
+        } else if(i == 3) {
+            ans += solve(n-1, arr, 2) % big;
+            ans += solve(n-1, arr, 4) % big;
+        } else if(i == 4) {
+            ans = solve(n-1, arr, 0) % big;
+        } else {
+            for(int k=0; k<5; k++) {
+                ans += solve(n-1, arr, k);
+                ans %= big;
+            }
+        }
+        
+        return (int)(ans % big);
+    }
+}
+*/
+
+// Memoization
+// O(n*k) || O(n*k) + O(n**2)
+/*
+class Solution {
+    final int big = (int)(1e9+7);
+    
     public int countVowelPermutation(int n) {
         Integer[][]dp = new Integer[n+1][5];
         char[]arr = new char[]{'a', 'e', 'i', 'o', 'u'};
         return solve(n, arr, -1, dp);
     }
+    
     public int solve(int n, char[]arr, int i, Integer[][]dp) {
         if(n == 0) return 1;
         
@@ -36,5 +83,41 @@ class Solution {
         
         if(i == -1) return (int)(ans % big);
         return dp[n][i] = (int)(ans % big);
+    }
+}
+*/
+
+// Tabulation
+// O(n*k) || O(n*k)
+class Solution {
+    public int countVowelPermutation(int n) {
+        
+        long big = (long)(1e9 + 7);
+        long[][] dp = new long[n+1][6];
+        
+        Arrays.fill(dp[0], 1);
+        for(int len=1 ;len<=n ;len++) {
+            for(int last=0 ;last<=5 ;last++) {
+                long count = 0;
+                if(last == 2) 
+                    count = (count + dp[len - 1][1]) % big;
+                if(last == 1 || last == 3)
+                    count = (count + dp[len - 1][2]) % big;
+                if(last == 1 || last == 2 || last == 4 || last == 5)
+                    count = (count + dp[len - 1][3]) % big;
+                if(last == 3 || last == 5)
+                    count = (count + dp[len - 1][4]) % big;
+                if(last == 1)
+                    count = (count + dp[len - 1][5]) % big;
+                if(last == 0) {
+                    for(int i=1 ;i<=5 ;i++) {
+                        count = (count + dp[len - 1][i]) % big;
+                    }
+                }
+                
+                dp[len][last] = count % big;
+            }
+        }
+        return (int)dp[n][0];
     }
 }
