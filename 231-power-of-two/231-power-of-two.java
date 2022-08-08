57594b2f3aebe48d4565c82096d78afe8013fe54
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-08 15:52:23 +0530

    Time: 2 ms (71.70%), Space: 41.2 MB (58.10%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int x) {
         if(x <= 0) return false;
-        if(x - (x & -x) == 0) return true;
-        return false;
+        return (x - (x & -x) == 0);
     }
 }
