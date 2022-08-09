ade1d93a03a228e0e7a70dad63c8037ac83cd139
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-10 00:09:40 +0530

    Time: 1 ms (94.69%), Space: 44 MB (54.39%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.java
+++ 137-single-number-ii/137-single-number-ii.java
@@ -1,3 +1,5 @@
+// O(n*32) || O(1)
+/*
 class Solution {
     public int singleNumber(int[] nums) {
         int n = 0;
@@ -10,5 +12,25 @@ class Solution {
             if(counter % 3 == 1) n |= val;
         }
         return n;
+    }
+}
+*/
+
+class Solution {
+    public int singleNumber(int[] arr) {
+        int one = ~0;
+        int two = 0, three = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            int tnp1 = one & arr[i];
+            int tnp2 = two & arr[i];
+            int tnp3 = three & arr[i];
+
+            one = (one & ~tnp1) | tnp3;
+            two = (two & ~tnp2) | tnp1;
+            three = (three & ~tnp3) | tnp2;
+        }
+        
+        return two;
     }
 }
