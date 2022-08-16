aacfbe9081a6d1c68aeeff4c855616a407035974
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-16 22:45:33 +0530

    Time: 3 ms (89.80%), Space: 49.5 MB (74.79%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,3 +1,15 @@
+/*
+    We divide the two arrays int0 two new arrays such that the number of elements
+    in both new subarrays are (len[1]+len[2])/2.
+    
+    In case the length is odd, we let the second array contain the extra element.
+    
+    l1 is the last number from first parent array in the first child array.
+    l2 is the last number from second parent array in the first child array.
+    r1 is the first number from first parent array in the second child array.
+    r2 is the first number from second parent array in the second child array.
+*/
+/*
 class Solution {
     public double findMedianSortedArrays(int[] n1, int[] n2) {
         if(n1.length > n2.length) {
@@ -11,7 +23,7 @@ class Solution {
         
         while(lo <= hi) {
             int x = lo + (hi-lo)/2;
-            int y = (n1.length+n2.length)/2 - x;
+            int y = (n1.length+n2.length)/2 - x;        // letting the second arr contain the extra element
             
             int l1 = x == 0 ? Integer.MIN_VALUE:n1[x-1];
             int l2 = y == 0 ? Integer.MIN_VALUE:n2[y-1];
@@ -24,12 +36,48 @@ class Solution {
                 lo = x + 1;
             } else {
                 if((n1.length + n2.length) % 2 == 0) {
+                    // In case of even number of elements, we avg the two partition extremities.
                     return (double)(Math.max(l1, l2) + Math.min(r1, r2))/2;
                 } else {
+                    // In case of odd number of elements, the right subarray has greater number of elements.
                     return (double)Math.min(r1, r2);
                 }
             }
         }
         return -1d;
+    }
+}
+*/
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        
+        int m1 = (m+n+1)/2;
+        int m2 = (m+n+2)/2;
+        
+        int median1 = getKthElementFromMergedArray(nums1, nums2, m1, 0, 0);
+        int median2 = getKthElementFromMergedArray(nums1, nums2, m2, 0, 0);
+        
+        return ((double)median1 + median2)/2;
+    }
+    
+    public int getKthElementFromMergedArray(int[]nums1, int[]nums2, int k, int s1, int s2) {
+        if(s1 >= nums1.length) return nums2[s2+k-1];
+        if(s2 >= nums2.length) return nums1[s1+k-1];
+        if(k == 1) return Math.min(nums1[s1], nums2[s2]);
+        
+        int ele1 = Integer.MAX_VALUE;
+        int ele2 = Integer.MAX_VALUE;
+        
+        if(s1 + k/2-1 < nums1.length) ele1 = nums1[s1 + k/2 -1];
+        if(s2 + k/2-1 < nums2.length) ele2 = nums2[s2 + k/2 -1];
+        
+        if(ele1 < ele2) {
+            return getKthElementFromMergedArray(nums1, nums2, k-k/2, s1+k/2, s2);
+        } else {
+            return getKthElementFromMergedArray(nums1, nums2, k-k/2, s1, s2+k/2);
+        }
     }
 }
