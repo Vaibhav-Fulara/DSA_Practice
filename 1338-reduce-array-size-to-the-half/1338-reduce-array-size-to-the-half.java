cb617836c6a607e3f39692e5ce37a7fbbc83832b
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-18 15:02:56 +0530

    Time: 116 ms (35.27%), Space: 95.2 MB (22.77%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -2,14 +2,20 @@ class Solution {
     public int minSetSize(int[] arr) {
         Map<Integer, Integer>hm = new HashMap<>();
         for(int val:arr) hm.put(val, hm.getOrDefault(val, 0) + 1);
-        PriorityQueue<Integer>pq = new PriorityQueue<>((a,b) -> b-a);
-        for(int key:hm.values()) pq.add(key);
-        int size = 0;
+        
+        int[]a = new int[(int)1e5];
+        for(int val:hm.keySet()) a[hm.get(val)]++;
+        
+        int ans = 0;
         int sum = 0;
-        while(size < (arr.length - arr.length/2)) {
-            size += pq.remove();
-            sum ++;
+        for(int i=a.length-1; i >= 0; i--) {
+            while(a[i] != 0 && sum < arr.length/2) {
+                sum += i;
+                a[i]--;
+                ans++;
+            }
+            if(sum >= arr.length/2) return ans;
         }
-        return sum;
+        return -1;
     }
 }
