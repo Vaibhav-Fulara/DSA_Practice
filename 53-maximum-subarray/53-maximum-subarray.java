910a46071cd0530be6f8a658af4eb837617706bf
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-08-28 07:01:52 +0530

    Time: 1 ms (100.00%), Space: 73.1 MB (78.26%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,12 +1,13 @@
+// Kadanes algorithm
 class Solution {
-    public int maxSubArray(int[] arr) {
-        int sum = Integer.MIN_VALUE;
+    public int maxSubArray(int[]arr) {
+        int max = Integer.MIN_VALUE;
         int curr = 0;
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++) {
             curr += arr[i];
-            sum = Math.max(curr, sum);
-            if(curr<0) curr = 0;
+            max = Math.max(curr, max);
+            if(curr < 0) curr = 0;
         }
-        return sum;
+        return max;
     }
 }
