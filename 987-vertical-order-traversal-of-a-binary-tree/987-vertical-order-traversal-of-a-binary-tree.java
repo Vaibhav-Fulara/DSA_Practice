156b5e6daa5c4b4e186c245214e3ba2cc34d63e0
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-09-05 01:24:45 +0530

    Time: 2 ms (99.97%), Space: 42.3 MB (96.86%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,18 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>>ans = new ArrayList<>();
@@ -22,7 +7,7 @@ class Solution {
             if(a.hor == b.hor && a.vert == b.vert) return a.data.val - b.data.val;
             if(a.vert == b.vert) return a.hor - b.hor;
             return a.vert-b.vert;
-            });
+        });
         
         int lo = 0, hi = 0;
         Queue<pair>qu = new ArrayDeque<>();
@@ -62,33 +47,3 @@ class Solution {
         }
     }
 }
-
-
-
-
-//     static ArrayList<Integer> topView(Node root){
-//         ArrayList<Integer>ans = new ArrayList<>();
-//         HashMap<Integer, Integer>hm = new HashMap<>();
-//         Queue<pair>qu = new LinkedList<>();
-        
-//         int lo = 0, hi = 0;
-//         qu.add(new pair(root, 0));
-        
-//         while(qu.size()!=0){
-//             pair p = qu.poll();
-//             Node node = p.val;
-//             int idx = p.idx;
-            
-//             if(idx > hi) hi = idx;
-//             if(idx < lo) lo = idx;
-            
-//             if(!hm.containsKey(idx)) hm.put(idx, node.data);
-//             if(node.left != null) qu.add(new pair(node.left, idx-1));
-//             if(node.right != null) qu.add(new pair(node.right, idx+1));
-//         }
-        
-//         for(int i=lo; i<=hi; i++){
-//             ans.add(hm.get(i));
-//         }
-//         return ans;
-//     }
