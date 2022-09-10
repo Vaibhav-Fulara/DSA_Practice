9b5e438258fbcaad459a0963cbd05c13d1d3c777
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-09-10 18:07:39 +0530

    Time: 109 ms (8.12%), Space: 42.8 MB (48.29%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,3 +1,5 @@
+// Recurrence O(n**k) TLE
+/*
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int[][]dp = new int[prices.length][k+1];
@@ -14,5 +16,46 @@ class Solution {
         }
         max = Math.max(max, solve(arr, k, idx+1, dp));
         return dp[idx][k] = max;
+    }
+}
+*/
+// Memoizatoin O(n*k) AC
+/*
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int[][]dp = new int[prices.length][k+1];
+        for(int[]a:dp) Arrays.fill(a,-1);
+        return solve(prices, k, 0, dp);
+    }
+    public int solve(int[]arr, int k, int idx, int[][]dp) {
+        if(k == 0 || idx >= arr.length) return 0;
+        if(dp[idx][k] != -1) return dp[idx][k];
+        int max = Integer.MIN_VALUE;
+        int curr = -arr[idx];
+        for(int i = idx+1; i<arr.length; i++) {
+            max = Math.max(max, curr + arr[i] + solve(arr, k-1, i+1, dp));
+        }
+        max = Math.max(max, solve(arr, k, idx+1, dp));
+        return dp[idx][k] = max;
+    }
+}
+*/
+
+// Tabulation
+class Solution {
+    public int maxProfit(int K, int[] arr) {
+        int[][]dp = new int[arr.length+1][K+1];
+        for(int idx = arr.length-1; idx >= 0; idx--) {
+            for(int k = 1; k <= K; k++) {
+                int max = Integer.MIN_VALUE;
+                int curr = -arr[idx];
+                for(int i = idx+1; i<arr.length; i++) {
+                    max = Math.max(max, curr + arr[i] + dp[i+1][k-1]);
+                }
+                max = Math.max(max, dp[idx+1][k]);
+                dp[idx][k] = max;
+            }
+        }
+        return dp[0][K];
     }
 }
