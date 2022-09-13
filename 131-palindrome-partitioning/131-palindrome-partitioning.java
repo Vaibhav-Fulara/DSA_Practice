50e46b2166b84b2d9d8aca92b17fa2a333965f4b
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-09-13 11:40:33 +0530

    Time: 28 ms (19.97%), Space: 196.1 MB (26.48%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,26 +1,27 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>>ans = new ArrayList<>();
-        getPalindrome(ans, 0, s, new ArrayList<>());
+        solve(ans, new ArrayList<>(), s, 0);
         return ans;
     }
-    public void getPalindrome(List<List<String>>ans, int lo, String s, List<String>al){
-        if(lo == s.length()){
-            ans.add(new ArrayList(al));
+    public void solve(List<List<String>>ans, List<String>al, String s, int lo) {
+        if(lo == s.length()) {
+            ans.add(new ArrayList<>(al));
             return;
         }
-        for(int i=lo; i<s.length(); i++){
-            String curr = s.substring(lo, i+1);
-            if(palindrome(curr)){
-                al.add(curr);
-                getPalindrome(ans, i+1, s, al);
+        for(int i=lo+1; i<=s.length(); i++) {
+            if(isPalindrome(s.substring(lo, i))) {
+                al.add(s.substring(lo,i));
+                solve(ans, al, s, i);
                 al.remove(al.size()-1);
             }
         }
     }
-    public boolean palindrome(String str){
-        int i=0, j=str.length()-1;
-        while(i<j)  if(str.charAt(i++) != str.charAt(j--)) return false;
+    public boolean isPalindrome(String s) {
+        int i=0, j = s.length()-1;
+        while(i < j) {
+            if(s.charAt(i++) != s.charAt(j--)) return false;
+        }
         return true;
     }
 }
