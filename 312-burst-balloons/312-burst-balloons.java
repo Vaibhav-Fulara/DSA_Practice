38c2babc36c3bc58ef36cd57459f54c18abed750
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-09-17 00:05:21 +0530

    Time: 252 ms (13.35%), Space: 42.7 MB (80.45%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,66 +1,22 @@
-// Memoization
-/*
 class Solution {
     public int maxCoins(int[] nums) {
-        
-        // Attachin 1 to pre and post of the array
-        int[]arr = new int[nums.length+2];
-        int idx = 0;
-        arr[idx++] = 1;
-        for(int val:nums) arr[idx++] = val;
-        arr[idx] = 1;
-        
-        // Initializing the DP
+        int[]arr = new int[nums.length + 2];
+        int i = 0;
+        arr[i++] = 1;
+        for(int val:nums) arr[i++] = val;
+        arr[i] = 1;
         int[][]dp = new int[arr.length][arr.length];
-        for(int[]ar:dp) Arrays.fill(ar, -1);
-        
-        return partition(arr, dp, 1, nums.length);
+        for(int[]a:dp) Arrays.fill(a,-1);
+        return solve(arr, 1, arr.length-2, dp);
     }
-    
-    public int partition(int[]arr, int[][]dp, int i, int j){
-        
-        // If we have overlapping subprobs, we return 0
-        
-        if(i > j) return dp[i][j] = 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        // We start from 1 balloon and go on upto the n balloons. The reverse would have
-        // led to dependant subproblems.
-        
+    public int solve(int[]arr, int lo, int hi, int[][]dp) {
+        if(lo > hi) return 0;
+        if(lo == hi) return arr[lo-1]*arr[lo]*arr[lo+1];
+        if(dp[lo][hi] != -1) return dp[lo][hi];
         int max = Integer.MIN_VALUE;
-        for(int k=i; k<=j; k++)
-            max = Math.max(max, arr[i-1]*arr[j+1]*arr[k] 
-                           + partition(arr, dp, i, k-1) 
-                           + partition(arr, dp, k+1, j));
-        return dp[i][j] = max;
-    }
-}
-*/
-
-class Solution {
-    public int maxCoins(int[] nums) {
-        
-        // Attachin 1 to pre and post of the array
-        int[]arr = new int[nums.length+2];
-        int idx = 0;
-        arr[idx++] = 1;
-        for(int val:nums) arr[idx++] = val;
-        arr[idx] = 1;
-        
-        // Initializing the DP
-        int[][]dp = new int[arr.length][arr.length];
-        
-        for(int i=nums.length; i>0; i--){
-            for(int j= i; j<=nums.length; j++){
-                int max = Integer.MIN_VALUE;
-                for(int k=i; k<=j; k++)
-                    max = Math.max(max, arr[i-1]*arr[j+1]*arr[k] 
-                                   + dp[i][k-1] + dp[k+1][j]);
-                dp[i][j] = max;
-            }
+        for(int idx=lo; idx<=hi; idx++) {
+            max = Math.max(max, arr[lo-1]*arr[idx]*arr[hi+1] + solve(arr, lo, idx-1, dp) + solve(arr, idx+1, hi, dp));
         }
-        
-        return dp[1][nums.length];
+        return dp[lo][hi] = max;
     }
 }
