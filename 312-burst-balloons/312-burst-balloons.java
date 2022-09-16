be3fa78a06b5289c0e41cecba63deaae1994e4ec
vaibhavfulara <82991892+Vaibhav-Fulara@users.noreply.github.com>
2022-09-17 00:15:05 +0530

    Time: 57 ms (86.38%), Space: 43.3 MB (40.91%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,21 +1,26 @@
 class Solution {
     public int maxCoins(int[] nums) {
+        
+        // Build an untruncated array
         int[]arr = new int[nums.length + 2];
         int i = 0;
         arr[i++] = 1;
         for(int val:nums) arr[i++] = val;
         arr[i] = 1;
+        
+        // create a dp
         int[][]dp = new int[arr.length][arr.length];
-        for(int[]a:dp) Arrays.fill(a,-1);
-        return solve(arr, 1, arr.length-2, dp);
-    }
-    public int solve(int[]arr, int lo, int hi, int[][]dp) {
-        if(lo > hi) return 0;
-        if(dp[lo][hi] != -1) return dp[lo][hi];
-        int max = Integer.MIN_VALUE;
-        for(int idx=lo; idx<=hi; idx++) {
-            max = Math.max(max, arr[lo-1]*arr[idx]*arr[hi+1] + solve(arr, lo, idx-1, dp) + solve(arr, idx+1, hi, dp));
+        
+        for(int lo = arr.length-2; lo > 0; lo--) {
+            for(int hi = lo; hi < arr.length-1; hi++) {
+                int max = Integer.MIN_VALUE;
+                for(int idx=lo; idx<=hi; idx++) {
+                    max = Math.max(max, arr[lo-1]*arr[idx]*arr[hi+1] + dp[lo][idx-1] + dp[idx+1][hi]);
+                }
+                dp[lo][hi] = max;
+            }
         }
-        return dp[lo][hi] = max;
+        
+        return dp[1][arr.length-2];
     }
 }
